af49edb2174d4a12b4340459facc8bdbb470fe76
iketheadore <heaven.mao@gmail.com>
2016-06-16 13:32:47 +0800

    change the name of Coin to CoinType in AddressEntry struct

--- src/server/wallet/wallet.go
+++ src/server/wallet/wallet.go
@@ -16,15 +16,15 @@ const (
 )
 
 type AddressEntry struct {
-	Coin    CoinType // coin type
-	Address string   // address
-	Pubkey  string   // publich key
-	Seckey  string   // private key
+	CoinType CoinType // coin type
+	Address  string   // address
+	Pubkey   string   // publich key
+	Seckey   string   // private key
 }
 
 type Wallet interface {
 	SetID(id string)
 	GetID() string
 	NewAddresses(coinType CoinType, num int) []AddressEntry
-	GetBalance(addr string) (string, error)
+	GetBalance(addr AddressEntry) (string, error)
 }
