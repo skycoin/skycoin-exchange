cc0660fb71cc79e4c4ea560bcf829eec29b3dd05
iketheadore <heaven.mao@gmail.com>
2016-06-22 15:59:57 +0800

    add lock for account manager

--- src/server/acount_state.go
+++ src/server/acount_state.go
@@ -2,24 +2,24 @@ package skycoin_exchange
 
 import (
 	"errors"
+	"sync"
 
 	"github.com/skycoin/skycoin-exchange/src/server/wallet"
 	"github.com/skycoin/skycoin/src/cipher"
 )
 
-type AccountIdentifier cipher.Address
+type AccountID cipher.Address
 
 // set of all users
 type AccountManager struct {
-	Accounts map[AccountIdentifier]*AccountState
+	Accounts map[AccountID]*AccountState
+	lck      sync.Mutex
 	//AccountMap map[cipher.Address]uint64
 }
 
 //store state of user on server
 type AccountState struct {
-	//AccountId  uint64
-	AccountID AccountIdentifier
-
+	ID      AccountID
 	Balance map[string]uint64
 	//Bitcoin balance in satoshis
 	//Skycoin balance in drops
@@ -28,15 +28,20 @@ type AccountState struct {
 	//Inc2 uint64 //set to last change. Associatd with global event id
 }
 
-func (self *AccountManager) GetAccount(addr AccountIdentifier) (*AccountState, error) {
+// GetAccount, return the copy value of speicific account.
+func (self *AccountManager) GetAccount(addr AccountID) (AccountState, error) {
+	self.lck.Lock()
+	self.lck.Unlock()
 	if account, ok := self.Accounts[addr]; ok {
-		return account, nil
+		return *account, nil
 	} else {
 		return nil, errors.New("Account does not exist")
 	}
 }
 
-func (self *AccountManager) CreateAccount(addr AccountIdentifier) (*AccountState, error) {
+func (self *AccountManager) CreateAccount(addr AccountID) (AccountState, error) {
+	self.lck.Lock()
+	self.lck.Unlock()
 	if _, ok := self.Accounts[addr]; ok == true {
 		return nil, errors.New("Account already exists")
 	}
@@ -46,7 +51,7 @@ func (self *AccountManager) CreateAccount(addr AccountIdentifier) (*AccountState
 	return act, nil
 }
 
-func newAccount(addr AccountIdentifier) *AccountState {
+func newAccount(addr AccountID) *AccountState {
 	return &AccountState{
 		AccountID: addr,
 		Balance: map[string]uint64{
