3533f9afca47cd13b3595c378bb8d6019f3f0c48
iketheadore <heaven.mao@gmail.com>
2016-06-29 17:10:56 +0800

    update the main package for testing.

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,26 +1,11 @@
 package main
 
-//"encoding/json"
-//"errors"
-// "fmt"
-//"github.com/go-goodies/go_oops"
-//"github.com/l3x/jsoncfgo"
-// "html/template"
-//"io/ioutil"
-// "log"
-// "net/http"
-//"regexp"
-// "github.com/skycoin/skycoin/src/cipher"
-// "os"
-
-import (
-	skycoin_exchange "github.com/skycoin/skycoin-exchange/src/server"
-)
+import "github.com/skycoin/skycoin-exchange/src/server"
 
 func main() {
-
-	server := skycoin_exchange.Server
-
-	server.Init()
-	server.Run()
+	cfg := server.Config{
+		Port:          6060,
+		WalletDataDir: ""}
+	s := server.New(cfg)
+	s.Run()
 }
