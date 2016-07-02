7d1accab9b292c7b619080911a58ef263b7de28c
iketheadore <heaven.mao@gmail.com>
2016-07-02 16:12:25 +0800

    add test cases for testing AuthRequired function.

--- src/server/account_test.go
+++ src/server/account_test.go
@@ -8,10 +8,12 @@ import (
 	"fmt"
 	"io"
 	"io/ioutil"
+	"net/http/httptest"
 	"strings"
 	"testing"
 	"time"
 
+	"github.com/codahale/chacha20"
 	"github.com/skycoin/skycoin-exchange/src/server/account"
 	"github.com/skycoin/skycoin-exchange/src/server/wallet"
 	"github.com/skycoin/skycoin/src/cipher"
@@ -61,21 +63,38 @@ func (fa FakeAccount) GetNonceKey() account.NonceKey {
 }
 
 func (fa FakeAccount) Encrypt(r io.Reader) ([]byte, error) {
-	d, _ := ioutil.ReadAll(r)
-	return d, nil
+	d, err := ioutil.ReadAll(r)
+	if err != nil {
+		return []byte{}, err
+	}
+
+	data := make([]byte, len(d))
+	c, err := chacha20.New(fa.Nk.Key, fa.Nk.Nonce)
+	if err != nil {
+		return []byte{}, err
+	}
+	c.XORKeyStream(data, d)
+	return data, nil
 }
 
 func (fa FakeAccount) Decrypt(r io.Reader) ([]byte, error) {
-	d, _ := ioutil.ReadAll(r)
-	return d, nil
+	d, err := ioutil.ReadAll(r)
+	if err != nil {
+		return []byte{}, err
+	}
+
+	data := make([]byte, len(d))
+	c, err := chacha20.New(fa.Nk.Key, fa.Nk.Nonce)
+	if err != nil {
+		return []byte{}, err
+	}
+	c.XORKeyStream(data, d)
+	return data, nil
 }
 
 func (fa FakeAccount) IsExpired() bool {
-	return false
-}
-
-func (fa *FakeAccount) UpdateNonceKeyExpireTime(t time.Time) {
-
+	d := time.Now().Unix() - fa.Nk.Expire_at.Unix()
+	return d >= 0
 }
 
 func (fs *FakeServer) CreateAccountWithPubkey(pk cipher.PubKey) (account.Accounter, error) {
@@ -97,7 +116,7 @@ func (fs *FakeServer) Run() {
 }
 
 func (fs FakeServer) GetNonceKeyLifetime() time.Duration {
-	return time.Duration(10 * 60)
+	return time.Second * time.Duration(10*60)
 }
 
 var pubkey string = "02c0a2e523be9234028874a08d001d422a1a191af910b8b4c315ab7fd59223726c"
@@ -164,7 +183,7 @@ func TestCreateAccountFaildBindWallet(t *testing.T) {
 	assert.Equal(t, w.Code, 501)
 }
 
-func TestAuthorize(t *testing.T) {
+func TestAuth(t *testing.T) {
 	p, s := cipher.GenerateKeyPair()
 	pubkey := fmt.Sprintf("%x", p)
 	svr := FakeServer{
@@ -189,7 +208,7 @@ func TestAuthorize(t *testing.T) {
 	assert.Equal(t, bytes.Equal(svr.A.GetNonceKey().Key, key), true)
 }
 
-func TestAuthorizeInvalidPubkey(t *testing.T) {
+func TestAuthInvalidPubkey(t *testing.T) {
 	svr := FakeServer{
 		A: &FakeAccount{
 			ID:      pubkey,
@@ -204,7 +223,7 @@ func TestAuthorizeInvalidPubkey(t *testing.T) {
 	assert.Equal(t, w.Code, 400)
 }
 
-func TestAuthorizeUnknowID(t *testing.T) {
+func TestAuthUnknowID(t *testing.T) {
 	p, _ := cipher.GenerateKeyPair()
 	client_pubkey := fmt.Sprintf("%x", p)
 	svr := FakeServer{
@@ -221,7 +240,52 @@ func TestAuthorizeUnknowID(t *testing.T) {
 	assert.Equal(t, w.Code, 404)
 }
 
-func TestGetDepositAddressWithoutAuth(t *testing.T) {
+func TestAuthReqRequest(t *testing.T) {
+	p, _ := cipher.GenerateKeyPair()
+	client_pubkey := fmt.Sprintf("%x", p)
+	svr := FakeServer{
+		A: &FakeAccount{
+			ID:      pubkey,
+			WltID:   "",
+			Addr:    "16VV1EbKHK7e3vJu4rhq2dJwegDcbaCcma",
+			Balance: account.Balance(0),
+		},
+	}
+
+	jsonStr := fmt.Sprintf(`{"pubkey": "%s"}`, client_pubkey)
+	w := MockServer(&svr, HttpRequestCase("POST", "/api/v1/authorization", strings.NewReader(jsonStr[:10])))
+	assert.Equal(t, w.Code, 400)
+}
+
+func TestAuthReqExpire(t *testing.T) {
+	p, _ := cipher.GenerateKeyPair()
+	pubkey := fmt.Sprintf("%x", p)
+	svr := FakeServer{
+		A: &FakeAccount{
+			ID:    pubkey,
+			WltID: "test.wlt",
+			Addr:  "16VV1EbKHK7e3vJu4rhq2dJwegDcbaCcma",
+			// the account will expire after 1 second
+			Nk: account.NonceKey{Key: key, Nonce: nonce, Expire_at: time.Now().Add(time.Millisecond * time.Duration(200))},
+		},
+	}
+
+	// send get deposite address request
+	dar := DepositAddressRequest{
+		AccountID: pubkey, // NOTE: different key
+		CoinType:  "bitcoin",
+	}
+
+	ct := dar.MustToContentRequest(key, nonce)
+	ctd, err := json.Marshal(ct)
+	assert.Nil(t, err)
+
+	time.Sleep(time.Millisecond * time.Duration(300))
+	w := MockServer(&svr, HttpRequestCase("POST", "/api/v1/account/deposit_address", bytes.NewBuffer(ctd)))
+	assert.Equal(t, w.Code, 401)
+}
+
+func TestUnAuth(t *testing.T) {
 	svr := FakeServer{
 		A: &FakeAccount{
 			ID:      pubkey,
@@ -238,6 +302,111 @@ func TestGetDepositAddressWithoutAuth(t *testing.T) {
 	cr := dar.MustToContentRequest(make([]byte, 32), make([]byte, 8))
 	d, _ := json.Marshal(cr)
 	w := MockServer(&svr, HttpRequestCase("POST", "/api/v1/account/deposit_address", bytes.NewBuffer(d)))
+	assert.Equal(t, w.Code, 401)
+}
+
+var key []byte = []byte{0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+	0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00}
+var nonce []byte = []byte{0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00}
+
+func TestAuthReqBadRequest(t *testing.T) {
+	p, _ := cipher.GenerateKeyPair()
+	pubkey := fmt.Sprintf("%x", p)
+	svr := FakeServer{
+		A: &FakeAccount{
+			ID:    pubkey,
+			WltID: "test.wlt",
+			Addr:  "16VV1EbKHK7e3vJu4rhq2dJwegDcbaCcma",
+			Nk:    account.NonceKey{Key: key, Nonce: nonce, Expire_at: time.Now().Add(time.Second * time.Duration(10))},
+		},
+	}
+
+	// send get deposite address request
+	dar := DepositAddressRequest{
+		AccountID: pubkey,
+		CoinType:  "bitcoin",
+	}
+
+	ct := dar.MustToContentRequest(key, nonce)
+	ctd, err := json.Marshal(ct)
+	assert.Nil(t, err)
+	w := MockServer(&svr, HttpRequestCase("POST", "/api/v1/account/deposit_address", bytes.NewBuffer(ctd[:10]))) // NOTE: Bad request
+	assert.Equal(t, w.Code, 400)
+}
+
+func TestAuthReqBadPubkey(t *testing.T) {
+	p, _ := cipher.GenerateKeyPair()
+	pubkey := fmt.Sprintf("%x", p)
+	svr := FakeServer{
+		A: &FakeAccount{
+			ID:    pubkey,
+			WltID: "test.wlt",
+			Addr:  "16VV1EbKHK7e3vJu4rhq2dJwegDcbaCcma",
+			Nk:    account.NonceKey{Key: key, Nonce: nonce, Expire_at: time.Now().Add(time.Second * time.Duration(10))},
+		},
+	}
+
+	// send get deposite address request
+	dar := DepositAddressRequest{
+		AccountID: errPubkey, // NOTE: bad key
+		CoinType:  "bitcoin",
+	}
+
+	ct := dar.MustToContentRequest(key, nonce)
+	ctd, err := json.Marshal(ct)
+	assert.Nil(t, err)
+	w := MockServer(&svr, HttpRequestCase("POST", "/api/v1/account/deposit_address", bytes.NewBuffer(ctd)))
+	assert.Equal(t, w.Code, 400)
+}
+
+func TestAuthReqBadID(t *testing.T) {
+	p, _ := cipher.GenerateKeyPair()
+	cli_pubkey := fmt.Sprintf("%x", p)
+	svr := FakeServer{
+		A: &FakeAccount{
+			ID:    pubkey,
+			WltID: "test.wlt",
+			Addr:  "16VV1EbKHK7e3vJu4rhq2dJwegDcbaCcma",
+		},
+	}
+
+	// send get deposite address request
+	dar := DepositAddressRequest{
+		AccountID: cli_pubkey, // NOTE: different key
+		CoinType:  "bitcoin",
+	}
+
+	ct := dar.MustToContentRequest(key, nonce)
+	ctd, err := json.Marshal(ct)
+	assert.Nil(t, err)
+	w := MockServer(&svr, HttpRequestCase("POST", "/api/v1/account/deposit_address", bytes.NewBuffer(ctd)))
+	assert.Equal(t, w.Code, 404)
+}
+
+func TestAuthReqBadNonceKey(t *testing.T) {
+	p, _ := cipher.GenerateKeyPair()
+	pubkey := fmt.Sprintf("%x", p)
+	svr := FakeServer{
+		A: &FakeAccount{
+			ID:    pubkey,
+			WltID: "test.wlt",
+			Addr:  "16VV1EbKHK7e3vJu4rhq2dJwegDcbaCcma",
+			Nk:    account.NonceKey{Key: key, Nonce: nonce, Expire_at: time.Now().Add(time.Second * time.Duration(10))},
+		},
+	}
+
+	// send get deposite address request
+	dar := DepositAddressRequest{
+		AccountID: pubkey,
+		CoinType:  "bitcoin",
+	}
+	errKey := make([]byte, 32)
+	copy(errKey, key)
+	errKey[0] = 0x98
+	ct := dar.MustToContentRequest(errKey, nonce)
+	ctd, err := json.Marshal(ct)
+	assert.Nil(t, err)
+	w := MockServer(&svr, HttpRequestCase("POST", "/api/v1/account/deposit_address", bytes.NewBuffer(ctd)))
 	assert.Equal(t, w.Code, 401)
 }
 
@@ -278,11 +447,16 @@ func TestCreateAddress(t *testing.T) {
 	nonce, err := hex.DecodeString(ar.Nonce)
 	assert.Nil(t, err)
 	ct := dar.MustToContentRequest(key, nonce)
-
 	ctd, err := json.Marshal(ct)
 	assert.Nil(t, err)
+
 	w = MockServer(&svr, HttpRequestCase("POST", "/api/v1/account/deposit_address", bytes.NewBuffer(ctd)))
 	assert.Equal(t, w.Code, 201)
+}
+
+func PrintResponse(w *httptest.ResponseRecorder) {
+	d, _ := ioutil.ReadAll(w.Body)
+	fmt.Println(string(d))
 }
 
 //
