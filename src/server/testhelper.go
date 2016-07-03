06526b7bc9356061ec60f9f69c11fd70c34e5793
iketheadore <heaven.mao@gmail.com>
2016-07-03 11:53:56 +0800

    move fake server and account to testhelper.go

--- src/server/testhelper.go
+++ src/server/testhelper.go
@@ -1,11 +1,19 @@
 package server
 
 import (
+	"errors"
+	"fmt"
 	"io"
+	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
+	"time"
 
+	"github.com/codahale/chacha20"
 	"github.com/gin-gonic/gin"
+	"github.com/skycoin/skycoin-exchange/src/server/account"
+	"github.com/skycoin/skycoin-exchange/src/server/wallet"
+	"github.com/skycoin/skycoin/src/cipher"
 )
 
 // CaseHandler represents one test case, which will be invoked by MockServer.
@@ -37,4 +45,107 @@ func HttpRequestCase(method string, url string, body io.Reader) CaseHandler {
 		}
 		return w, r
 	}
+}
+
+// FakeAccount for mocking various account state.
+type FakeAccount struct {
+	ID      string
+	WltID   string
+	Addr    string
+	Nk      account.NonceKey
+	Balance uint64
+}
+
+// FakeServer for mocking various server status.
+type FakeServer struct {
+	A account.Accounter
+}
+
+func (fa FakeAccount) GetWalletID() string {
+	return fa.WltID
+}
+
+func (fa FakeAccount) GetAccountID() account.AccountID {
+	d, err := cipher.PubKeyFromHex(fa.ID)
+	if err != nil {
+		panic(err)
+	}
+	return account.AccountID(d)
+}
+
+func (fa FakeAccount) GetNewAddress(ct wallet.CoinType) string {
+	return fa.Addr
+}
+
+func (fa FakeAccount) GetBalance(ct wallet.CoinType) uint64 {
+	return fa.Balance
+}
+
+func (fa *FakeAccount) SetNonceKey(nk account.NonceKey) {
+	fa.Nk = nk
+}
+
+func (fa FakeAccount) GetNonceKey() account.NonceKey {
+	return fa.Nk
+}
+
+func (fa FakeAccount) Encrypt(r io.Reader) ([]byte, error) {
+	d, err := ioutil.ReadAll(r)
+	if err != nil {
+		return []byte{}, err
+	}
+
+	data := make([]byte, len(d))
+	c, err := chacha20.New(fa.Nk.Key, fa.Nk.Nonce)
+	if err != nil {
+		return []byte{}, err
+	}
+	c.XORKeyStream(data, d)
+	return data, nil
+}
+
+func (fa FakeAccount) Decrypt(r io.Reader) ([]byte, error) {
+	d, err := ioutil.ReadAll(r)
+	if err != nil {
+		return []byte{}, err
+	}
+
+	data := make([]byte, len(d))
+	c, err := chacha20.New(fa.Nk.Key, fa.Nk.Nonce)
+	if err != nil {
+		return []byte{}, err
+	}
+	c.XORKeyStream(data, d)
+	return data, nil
+}
+
+func (fa FakeAccount) IsExpired() bool {
+	d := time.Now().Unix() - fa.Nk.Expire_at.Unix()
+	return d >= 0
+}
+
+func (fa FakeAccount) GenerateWithdrawTx(coins uint64, coinType wallet.CoinType) ([]byte, error) {
+	return []byte{}, nil
+}
+
+func (fs *FakeServer) CreateAccountWithPubkey(pk cipher.PubKey) (account.Accounter, error) {
+	if fs.A.GetWalletID() == "" {
+		return nil, fmt.Errorf("create wallet failed")
+	}
+	return fs.A, nil
+}
+
+func (fs *FakeServer) GetAccount(id account.AccountID) (account.Accounter, error) {
+	if fs.A != nil && fs.A.GetAccountID() == id {
+		return fs.A, nil
+	}
+	return nil, errors.New("account not found")
+}
+
+func (fs *FakeServer) Run() {
+
+}
+
+func (fs FakeServer) GetNonceKeyLifetime() time.Duration {
+	return time.Second * time.Duration(10*60)
 }
