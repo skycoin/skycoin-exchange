5484a9d9593f3d2cec0b9c5a363335b644919e17
iketheadore <heaven.mao@gmail.com>
2016-07-04 22:25:24 +0800

    update auth procedure.

--- src/server/message.go
+++ src/server/message.go
@@ -1,24 +1,28 @@
 package server
 
 import (
-	"bytes"
 	"encoding/json"
 	"time"
 
-	"github.com/skycoin/skycoin-exchange/src/server/account"
+	"github.com/skycoin/skycoin/src/cipher"
 )
 
-type AuthRequest struct {
-	Pubkey string `json:"pubkey"`
+type EncryptedData struct {
+	Data []byte `json:"data"`
 }
 
-type AuthResponse struct {
-	Pubkey string `json:"pubkey"`
-	Nonce  string `json:"nonce"`
-}
+// type AuthRequest struct {
+// 	Pubkey string `json:"pubkey"`
+// }
+//
+// type AuthResponse struct {
+// 	Pubkey string `json:"pubkey"`
+// 	Nonce  string `json:"nonce"`
+// }
 
 type ContentRequest struct {
 	AccountID string `json:"account_id"`
+	Nonce     []byte `json:"nonce"`
 	Data      []byte `json:"data"`
 }
 
@@ -30,11 +34,10 @@ type ContentResponse struct {
 
 type CreateAccountRequest struct {
 	Pubkey string `json:"pubkey"`
-	Data   []byte `json:"data"`
 }
 
 type CreateAccountResponse struct {
-	Succress  bool      `json:"success"`
+	Success   bool      `json:"success"`
 	AccountID string    `json:"account_id"`
 	CreatedAt time.Time `json:"created_at"`
 }
@@ -45,6 +48,7 @@ type DepositAddressRequest struct {
 }
 
 type DepositAddressResponse struct {
+	Success     bool   `json:"success"`
 	AccountID   string `json:"account_id"`
 	DepositAddr string `json:"deposit_address"`
 }
@@ -61,19 +65,64 @@ type WithdrawResponse struct {
 	Tx        []byte `json:"tx"` // signed transaction
 }
 
-func (wr WithdrawResponse) MustToContentResponse(a account.Accounter) ContentResponse {
-	d, err := json.Marshal(wr)
+func MustToContentRequest(r interface{}, id string, key []byte) ContentRequest {
+	d, err := json.Marshal(r)
 	if err != nil {
 		panic(err)
 	}
-
-	data, _ := a.Encrypt(bytes.NewBuffer(d))
-
-	return ContentResponse{
-		Success: true,
-		Data:    data,
+	nonce := cipher.RandByte(8)
+	data, err := Encrypt(d, key, nonce)
+	if err != nil {
+		panic(err)
+	}
+	return ContentRequest{
+		AccountID: id,
+		Data:      data,
+		Nonce:     nonce,
 	}
 }
+
+// func (car CreateAccountRequest) MustToContentRequest(id string, key []byte) ContentRequest {
+// }
+//
+// func (dar DepositAddressRequest) MustToContentResponse(id string, key []byte) ContentRequest {
+// 	d, err := json.Marshal(dar)
+// 	if err != nil {
+// 		panic(err)
+// 	}
+// 	nonce := cipher.RandByte(8)
+// 	data, err := Encrypt(d, key, nonce)
+// 	if err != nil {
+// 		panic(err)
+// 	}
+// 	return ContentRequest{
+// 		AccountID: id,
+// 		Data:      data,
+// 		Nonce:     nonce,
+// 	}
+// }
+
+func (cr ContentRequest) MustJson() []byte {
+	d, err := json.Marshal(cr)
+	if err != nil {
+		panic(err)
+	}
+	return d
+}
+
+// func (wr WithdrawResponse) MustToContentResponse(a account.Accounter) ContentResponse {
+// 	d, err := json.Marshal(wr)
+// 	if err != nil {
+// 		panic(err)
+// 	}
+//
+// 	data, _ := a.Encrypt(bytes.NewBuffer(d))
+//
+// 	return ContentResponse{
+// 		Success: true,
+// 		Data:    data,
+// 	}
+// }
 
 // MustToContentRequest convert DepositAddressRequest to ContentRequest
 func (dar DepositAddressRequest) MustToContentRequest(key []byte, nonce []byte) ContentRequest {
