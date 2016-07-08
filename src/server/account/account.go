e747957e9981d38c9ae5ffb3edc5b4546b64a247
iketheadore <heaven.mao@gmail.com>
2016-07-08 11:15:50 +0800

    add chooseUtxos

--- src/server/account/account.go
+++ src/server/account/account.go
@@ -3,8 +3,8 @@ package account
 import (
 	"errors"
 	"fmt"
+	"sort"
 	"sync"
-	"time"
 
 	bitcoin "github.com/skycoin/skycoin-exchange/src/server/coin_interface/bitcoin"
 	"github.com/skycoin/skycoin-exchange/src/server/wallet"
@@ -14,11 +14,13 @@ import (
 type AccountID cipher.PubKey
 
 type Accounter interface {
-	GetWalletID() string                                                               // return the wallet id.
-	GetID() AccountID                                                                  // return the account id.
-	GetNewAddress(ct wallet.CoinType) string                                           // return new address for receiveing coins.
-	GetBalance(ct wallet.CoinType) uint64                                              // return the current balance.
-	GenerateWithdrawTx(ct wallet.CoinType, outAddrs []bitcoin.OutAddr) ([]byte, error) // account generate withdraw transaction.
+	GetWalletID() string                                                                             // return the wallet id.
+	GetID() AccountID                                                                                // return the account id.
+	GetNewAddress(ct wallet.CoinType) string                                                         // return new address for receiveing coins.
+	GetBalance(ct wallet.CoinType) uint64                                                            // return the account's balance.
+	GetAddressBalance(addr string) (uint64, error)                                                   // return the address's balance
+	GenerateWithdrawlTx(ct wallet.CoinType, Amount uint64, toAdd string, fee uint64) ([]byte, error) // account generate withdraw transaction.
+	GetAddressEntries(coinType wallet.CoinType) ([]wallet.AddressEntry, error)
 }
 
 // ExchangeAccount maintains the account state
@@ -30,13 +32,16 @@ type ExchangeAccount struct {
 	wlt_mtx     sync.Mutex                 // mutex used to protect the wallet's conncurrent read and write.
 }
 
-// NonceKey used to encrypt and decrypt data,
-// key will become invalid when time exceed the specific time.
-type NonceKey struct {
-	Nonce     []byte
-	Key       []byte
-	Expire_at time.Time
+type addrBalance struct {
+	Addr    string
+	Balance uint64
 }
+
+type byBalance []addrBalance
+
+func (bb byBalance) Len() int           { return len(bb) }
+func (bb byBalance) Swap(i, j int)      { bb[i], bb[j] = bb[j], bb[i] }
+func (bb byBalance) Less(i, j int) bool { return bb[i].Balance < bb[j].Balance }
 
 // newExchangeAccount helper function for generating and initialize ExchangeAccount
 func newExchangeAccount(id AccountID, wltID string) ExchangeAccount {
@@ -81,6 +86,10 @@ func (self *ExchangeAccount) GetBalance(coinType wallet.CoinType) uint64 {
 	return self.balance[coinType]
 }
 
+func (self ExchangeAccount) GetAddressBalance(addr string) (uint64, error) {
+	return bitcoin.GetBalance([]string{addr})
+}
+
 // SetBalance update the balanace of specific coin.
 func (self *ExchangeAccount) setBalance(coinType wallet.CoinType, balance uint64) error {
 	self.balance_mtx.Lock()
@@ -93,44 +102,96 @@ func (self *ExchangeAccount) setBalance(coinType wallet.CoinType, balance uint64
 }
 
 // GenerateWithdrawTx
-func (self ExchangeAccount) GenerateWithdrawTx(coinType wallet.CoinType, outAddrs []bitcoin.OutAddr) ([]byte, error) {
+func (self ExchangeAccount) GenerateWithdrawlTx(coinType wallet.CoinType, amount uint64, toAddr string, fee uint64) ([]byte, error) {
 	// check if balance sufficient
 	bla := self.GetBalance(coinType)
-	var allCoins int64
-	for _, out := range outAddrs {
-		allCoins += out.Value
-	}
 
-	if bla < uint64(allCoins) {
+	if bla < (amount + fee) {
 		return []byte{}, errors.New("balance is not sufficient")
 	}
 
-	addrs, err := self.getAddressEntries(coinType)
+	// choose the appropriate utxos。
+	utxos, err := chooseUtxos(&self, coinType, amount)
 	if err != nil {
 		return []byte{}, err
 	}
 
-	// get utxos
-	utxos := []bitcoin.UtxoWithkey{}
+	// create change address
+	changeAddr := self.GetNewAddress(coinType)
+
+	outAddrs := []bitcoin.UtxoOut{
+		bitcoin.UtxoOut{Addr: toAddr, Value: amount},
+		bitcoin.UtxoOut{Addr: changeAddr, Value: bla - amount - fee},
+	}
+
 	switch coinType {
 	case wallet.Bitcoin:
-		for _, addrEntry := range addrs {
-			us := bitcoin.GetUnspentOutputsBlkChnInfo(addrEntry.Address)
-			for _, u := range us {
-				usk := bitcoin.BlkchnUtxoWithkey{
-					BlkChnUtxo: u,
-					Privkey:    addrEntry.Secret,
-				}
-				utxos = append(utxos, usk)
-			}
+		tx, err := bitcoin.NewTransaction(utxos, outAddrs)
+		if err != nil {
+			return []byte{}, errors.New("create bitcoin transaction failed")
 		}
-		return bitcoin.NewTransaction(utxos, outAddrs)
+		return bitcoin.DumpTxBytes(tx), nil
 	default:
 		return []byte{}, errors.New("unknow coin type")
 	}
+
+	// get utxos
+	// utxos := []bitcoin.UtxoWithkey{}
+	// switch coinType {
+	// case wallet.Bitcoin:
+	// 	for _, addrEntry := range addrs {
+	// 		us := bitcoin.GetUnspentOutputs(addrEntry.Address)
+	// 		usks := make([]bitcoin.UtxoWithkey, len(us))
+	// 		for i, u := range us {
+	// 			usk := bitcoin.NewUtxoWithKey(u, addrEntry.Secret)
+	// 			usks[i] = usk
+	// 		}
+	// 		utxos = append(utxos, usks...)
+	// 	}
+	// 	msgTx, err := bitcoin.NewTransaction(utxos, outAddrs)
+	// 	if err != nil {
+	// 		return []byte{}, errors.New("create bitcoin transaction faild")
+	// 	}
+	// 	return bitcoin.DumpTxBytes(msgTx), nil
+	// default:
+	// 	return []byte{}, errors.New("unknow coin type")
+	// }
 }
 
-func (self ExchangeAccount) getAddressEntries(coinType wallet.CoinType) ([]wallet.AddressEntry, error) {
+func chooseUtxos(a Accounter, coinType wallet.CoinType, amount uint64) ([]bitcoin.UtxoWithkey, error) {
+	addrEntries, err := a.GetAddressEntries(coinType)
+	utxoks := []bitcoin.UtxoWithkey{}
+	if err != nil {
+		return utxoks, errors.New("get account addresses failed")
+	}
+
+	addrBals := map[string]uint64{} // key: address, value: balance
+	addrKeys := map[string]string{} // key: address, value: private key
+	balList := []addrBalance{}
+
+	for _, addrEntry := range addrEntries {
+		// get the balance of addr
+		b, err := a.GetAddressBalance(addrEntry.Address)
+		if err != nil {
+			return utxoks, err
+		}
+		addrBals[addrEntry.Address] = b
+		addrKeys[addrEntry.Address] = addrEntry.Secret
+		balList = append(balList, addrBalance{Addr: addrEntry.Address, Balance: b})
+	}
+
+	// sort the bals list
+	sort.Sort(byBalance(balList))
+
+	return []bitcoin.UtxoWithkey{}, nil
+
+}
+
+// chooseUtxos choose utxos that can satisify the amount, and set the private key as well.
+// func (self ExchangeAccount) chooseUtxos(coinType wallet.CoinType, amount uint64) ([]bitcoin.UtxoWithkey, error) {
+// }
+
+func (self ExchangeAccount) GetAddressEntries(coinType wallet.CoinType) ([]wallet.AddressEntry, error) {
 	// get address list of this account
 	wlt, err := wallet.GetWallet(self.wltID)
 	if err != nil {
