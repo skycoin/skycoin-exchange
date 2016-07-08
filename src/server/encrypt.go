ca38a32b3650f58dd982ccf594fb84d880eb2e64
iketheadore <heaven.mao@gmail.com>
2016-07-08 22:36:02 +0800

    update the chacha20 encrypt and decrypt func

--- src/server/encrypt.go
+++ src/server/encrypt.go
@@ -1,16 +1,13 @@
 package server
 
-import "github.com/codahale/chacha20"
+import (
+	"errors"
 
-// Encrypt chacha20
-func Encrypt(data []byte, key []byte, nonce []byte) ([]byte, error) {
-	return encOrDec(data, key, nonce)
-}
+	"github.com/codahale/chacha20"
+	"github.com/skycoin/skycoin/src/cipher"
+)
 
-func Decrypt(data []byte, key []byte, nonce []byte) ([]byte, error) {
-	return encOrDec(data, key, nonce)
-}
-
+// chacha20
 func encOrDec(data []byte, key []byte, nonce []byte) ([]byte, error) {
 	e := make([]byte, len(data))
 	c, err := chacha20.New(key, nonce)
@@ -19,4 +16,28 @@ func encOrDec(data []byte, key []byte, nonce []byte) ([]byte, error) {
 	}
 	c.XORKeyStream(e, data)
 	return e, nil
+}
+
+func Decrypt(data []byte, serverPrivKey cipher.SecKey, clientPubkey cipher.PubKey, nonce []byte) (d []byte, err error) {
+	defer func() {
+		if r := recover(); r != nil {
+			err = errors.New("server decrypt faild")
+		}
+	}()
+
+	key := cipher.ECDH(clientPubkey, serverPrivKey)
+	d, err = encOrDec(data, key, nonce)
+	return
+}
+
+func Encrypt(data []byte, serverPrivKey cipher.SecKey, clientPubkey cipher.PubKey, nonce []byte) (d []byte, err error) {
+	defer func() {
+		if r := recover(); r != nil {
+			err = errors.New("server encrypt faild")
+		}
+	}()
+
+	key := cipher.ECDH(clientPubkey, serverPrivKey)
+	d, err = encOrDec(data, key, nonce)
+	return
 }
