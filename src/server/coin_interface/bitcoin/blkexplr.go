9ea61600cb518d2bb22213c6ee96b502ec57bf25
iketheadore <heaven.mao@gmail.com>
2016-07-10 14:57:03 +0800

    add method of getting utxos from blockexplorer.com, cause the utxo struct from blockchain.info doesn't contains address.

---
+++ src/server/coin_interface/bitcoin/blkexplr.go
@@ -0,0 +1,78 @@
+// get unspent output from blockexplorer.com
+package bitcoin_interface
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"strings"
+)
+
+type BlkExplrUtxoArray struct {
+	Data []BlkExplrUtxo
+}
+
+type BlkExplrUtxo struct {
+	Address      string  `json:"address"`
+	Txid         string  `json:"txid"`
+	Vout         uint32  `json:"vout"`
+	ScriptPubkey string  `json:"criptPubKey"`
+	Amount       float64 `json:"amount"`
+	Confirms     uint64  `json:"confirmations"`
+}
+
+func (be BlkExplrUtxo) GetTxid() string {
+	return be.Txid
+}
+
+func (be BlkExplrUtxo) GetVout() uint32 {
+	return be.Vout
+}
+
+func (be BlkExplrUtxo) GetAmount() uint64 {
+	return uint64(be.Amount * 100000000)
+}
+
+func (be BlkExplrUtxo) GetAddress() string {
+	return be.Address
+}
+
+// BlkChnUtxo with private key
+type BlkExplrUtxoWithkey struct {
+	BlkExplrUtxo
+	Privkey string
+}
+
+func (beu BlkExplrUtxoWithkey) GetPrivKey() string {
+	return beu.Privkey
+}
+
+func getUtxosBlkExplr(addrs []string) ([]Utxo, error) {
+	url := fmt.Sprintf("https://blockexplorer.com/api/addrs/%s/utxo", strings.Join(addrs, ","))
+	rsp, err := http.Get(url)
+	if err != nil {
+		return []Utxo{}, err
+	}
+
+	if rsp.StatusCode != 200 {
+		return []Utxo{}, errors.New("get unspent output from blockexplorer.com failed")
+	}
+	defer rsp.Body.Close()
+	data, err := ioutil.ReadAll(rsp.Body)
+	if err != nil {
+		return []Utxo{}, err
+	}
+	us := BlkExplrUtxoArray{}
+	err = json.Unmarshal(data, &us)
+	if err != nil {
+		return []Utxo{}, err
+	}
+
+	utxos := make([]Utxo, len(us.Data))
+	for i, u := range us.Data {
+		utxos[i] = &u
+	}
+	return utxos, nil
+}
