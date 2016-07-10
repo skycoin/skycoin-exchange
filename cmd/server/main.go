b285ebd0b2ec3b50cf27776f90888b2eeb923f47
iketheadore <heaven.mao@gmail.com>
2016-07-10 16:15:03 +0800

    use flag to setup server's config.

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,11 +1,35 @@
 package main
 
-import "github.com/skycoin/skycoin-exchange/src/server"
+import (
+	"flag"
+	"log"
+
+	"github.com/skycoin/skycoin-exchange/src/server"
+	"github.com/skycoin/skycoin/src/cipher"
+)
+
+var sk = "38d010a84c7b9374352468b41b076fa585d7dfac67ac34adabe2bbba4f4f6257"
+
+func registerFlags(cfg *server.Config) {
+	flag.IntVar(&cfg.Port, "port", 6060, "server listen port")
+	flag.IntVar(&cfg.Fee, "fee", 10000, "transaction fee in satoish")
+	flag.StringVar(&cfg.DataDir, "dataDir", "", "data directory")
+	flag.StringVar(&cfg.WalletName, "wltName", "server.wlt", "server's wallet file name")
+	flag.StringVar(&cfg.Seed, "s", "seed", "wallet's seed")
+	flag.IntVar(&cfg.UtxoPoolSize, "poolsize", 1000, "utxo pool size")
+}
 
 func main() {
-	cfg := server.Config{
-		Port:          6060,
-		WalletDataDir: ""}
+	cfg := server.Config{}
+	registerFlags(&cfg)
+	flag.Parse()
+
+	key, err := cipher.SecKeyFromHex(sk)
+	if err != nil {
+		log.Fatal(err)
+	}
+	cfg.Seckey = key
+
 	s := server.New(cfg)
 	s.Run()
 }
