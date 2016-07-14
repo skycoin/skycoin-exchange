40e082cb5cf8f3bb63aa4460685660eabb9c0e7f
iketheadore <heaven.mao@gmail.com>
2016-07-15 01:12:48 +0800

    change the client interface.

--- cmd/client/client.go
+++ cmd/client/client.go
@@ -1,9 +1,12 @@
 package main
 
-import (
-	"github.com/skycoin/skycoin-exchange/src/client"
+import "github.com/skycoin/skycoin-exchange/src/rpclient"
+
+const (
+	ServPubkey = "02942e46684114b35fe15218dfdc6e0d74af0446a397b8fcbf8b46fb389f756eb8"
 )
 
 func main() {
-	client.Run()
+	cli := rpclient.New("http://localhost:8080/api/v1", ServPubkey)
+	cli.Run(":6060")
 }
