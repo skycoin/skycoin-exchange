d16147b5b901d5d2f3d1fe6f75ee0d00e6ce40ed
iketheadore <heaven.mao@gmail.com>
2016-07-17 18:53:31 +0800

    move pp.content.proto to pp.encrypt.proto

--- src/pp/gen.sh
+++ src/pp/gen.sh
@@ -1,6 +1,6 @@
 protoc --go_out=./ pp.common.proto \
-  pp.content.proto \
+  pp.encrypt.proto \
   pp.account.proto \
   pp.deposit.proto \
   pp.withdrawal.proto \
-  pp.balance.proto 
+  pp.balance.proto
