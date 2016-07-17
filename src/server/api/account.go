1494b76ee44d4cf1520510c443cf55ebb9c1b6bb
iketheadore <heaven.mao@gmail.com>
2016-07-17 18:20:17 +0800

    api for handle accounts

---
+++ src/server/api/account.go
@@ -0,0 +1,56 @@
+package api
+
+import (
+	"encoding/json"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/golang/glog"
+	"github.com/skycoin/skycoin-exchange/src/pp"
+	"github.com/skycoin/skycoin-exchange/src/server/engine"
+	"github.com/skycoin/skycoin/src/cipher"
+)
+
+func getRequest(c *gin.Context, out interface{}) error {
+	d := c.MustGet("rawdata").([]byte)
+	return json.Unmarshal(d, out)
+}
+
+// CreateAccount create account with specific pubkey,
+func CreateAccount(ee engine.Exchange) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		errRlt := &pp.EmptyRes{}
+		for {
+			req := pp.CreateAccountReq{}
+			if err := getRequest(c, &req); err != nil {
+				glog.Error(err)
+				errRlt = pp.MakeErrResWithCode(pp.ErrCode_WrongRequest)
+				break
+			}
+
+			pubkey, err := cipher.PubKeyFromHex(req.GetPubkey())
+			if err != nil {
+				glog.Error(err)
+				errRlt = pp.MakeErrResWithCode(pp.ErrCode_WrongAccountId)
+				break
+			}
+
+			// create account with pubkey.
+			if _, err := ee.CreateAccountWithPubkey(pubkey); err != nil {
+				glog.Error(err)
+				errRlt = pp.MakeErrResWithCode(pp.ErrCode_WrongRequest)
+				break
+			}
+
+			res := pp.CreateAccountRes{
+				AccountId: req.Pubkey,
+				CreatedAt: pp.PtrInt64(time.Now().Unix()),
+			}
+
+			reply(c, res)
+			return
+		}
+
+		c.JSON(200, *errRlt)
+	}
+}
