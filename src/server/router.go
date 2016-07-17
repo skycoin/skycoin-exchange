7e299754813368d0f5197628f3d774a29b47a547
iketheadore <heaven.mao@gmail.com>
2016-07-17 18:49:56 +0800

    move api handlers to api package.

--- src/server/router.go
+++ src/server/router.go
@@ -1,19 +1,23 @@
 package server
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/skycoin/skycoin-exchange/src/server/api"
+	"github.com/skycoin/skycoin-exchange/src/server/engine"
+)
 
-func NewRouter(svr Server) *gin.Engine {
+func NewRouter(ee engine.Exchange) *gin.Engine {
 	r := gin.New()
 	r.Use(gin.Logger())
 	r.Use(gin.Recovery())
 
 	// the Authorize middle will decrypt the request, and encrypt the response.
-	v1 := r.Group("/api/v1", Authorize(svr))
+	v1 := r.Group("/api/v1", api.Authorize(ee))
 	{
-		v1.POST("/accounts", CreateAccount(svr)) // create account
-		v1.POST("/deposit_address", GetNewAddress(svr))
-		v1.POST("/account/withdrawal", Withdraw(svr))
-		v1.GET("/account/balance", GetBalance(svr))
+		v1.POST("/accounts", api.CreateAccount(ee)) // create account
+		v1.POST("/deposit_address", api.GetNewAddress(ee))
+		v1.POST("/account/withdrawal", api.Withdraw(ee))
+		v1.POST("/account/balance", api.GetBalance(ee))
 	}
 	return r
 }
