83294e2e9ceafa86ebc556372831278869c403cf
iketheadore <heaven.mao@gmail.com>
2016-07-24 13:04:37 +0800

    remove useless code.

--- src/server/coin_interface/bitcoin/bitcoin.go
+++ src/server/coin_interface/bitcoin/bitcoin.go
@@ -8,7 +8,6 @@ import (
 	"strconv"
 	"strings"
 
-	"log"
 	"net/http"
 
 	"github.com/skycoin/skycoin-exchange/src/server/coin_interface"
@@ -84,59 +83,6 @@ func NewUtxoWithKey(utxo Utxo, key string) UtxoWithkey {
 	}
 }
 
-type Manager struct {
-	WatchAddresses []string
-	UxStateMap     map[string]Utxo //keeps track of state
-}
-
-type UxMap map[string]Utxo
-
-//does querry/update
-func (self *Manager) UpdateOutputs() {
-	log.Println("Update outputs...")
-	//get all unspent outputs for all watch addresses
-	var list []Utxo
-	for _, addr := range self.WatchAddresses {
-		ux, err := GetUnspentOutputs([]string{addr})
-		if err != nil {
-			panic(err)
-		}
-		list = append(list, ux...)
-	}
-	latestUxMap := make(map[string]Utxo)
-	//do diff
-	for _, utxo := range list {
-		id := fmt.Sprintf("%s:%d", utxo.GetTxid(), utxo.GetVout())
-		latestUxMap[id] = utxo
-	}
-
-	//get new
-	NewUx := make(map[string]Utxo)
-	for id, utxo := range latestUxMap {
-		if _, ok := self.UxStateMap[id]; !ok {
-			NewUx[id] = utxo
-			log.Printf("New output Found:%+v\n", utxo)
-		}
-	}
-
-	// TODO:
-	// make sure outputs that exist, never disappear, without being spent
-	// means theft or blockchain fork
-
-	// look for ux that disappeared
-	// TODO: make sure output exists and has not disappeared, else panic mode
-	// TODO: output should still exist, even if not spendable
-	DisappearingUx := make(map[string]Utxo)
-	for id, utxo := range self.UxStateMap {
-		if _, ok := self.UxStateMap[id]; !ok {
-			DisappearingUx[id] = utxo
-			log.Printf("Output Disappered: %+v\n", utxo)
-		}
-	}
-
-	self.UxStateMap = latestUxMap
-}
-
 // getDataOfUrl, get data from specific URL.
 func getDataOfUrl(url string) ([]byte, error) {
 	resp, err := http.Get(url)
@@ -150,27 +96,4 @@ func getDataOfUrl(url string) ([]byte, error) {
 	}
 	resp.Body.Close()
 	return data, nil
-}
-
-func (self *Manager) Init() {
-	//UxStateMap     map[string]UnspentOutputJSON
-	self.WatchAddresses = make([]string, 0)
-	self.UxStateMap = make(map[string]Utxo)
-}
-
-func (self *Manager) AddWatchAddress(addr string) {
-	if AddressValid(addr) != nil {
-		log.Fatal("Address being added to watch list, must be valid")
-	}
-	self.WatchAddresses = append(self.WatchAddresses, addr)
-}
-
-func (self *Manager) Tick() {
-	self.UpdateOutputs()
-}
-
-//returns error if the address is invalid
-func AddressValid(address string) error {
-	//return errors.New("Address is invalid")
-	return nil
 }
