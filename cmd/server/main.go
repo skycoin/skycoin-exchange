4ede9969534f693732248187cdafcdef525e816f
iketheadore <heaven.mao@gmail.com>
2016-07-24 13:09:57 +0800

    fix bug about exchange server's data dir.

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -8,18 +8,16 @@ import (
 	"github.com/golang/glog"
 	"github.com/skycoin/skycoin-exchange/src/server"
 	"github.com/skycoin/skycoin/src/cipher"
-	"github.com/skycoin/skycoin/src/util"
 )
 
 var sk = "38d010a84c7b9374352468b41b076fa585d7dfac67ac34adabe2bbba4f4f6257"
 
 func registerFlags(cfg *server.Config) {
-	homeDir := util.UserHome()
 	flag.IntVar(&cfg.Port, "port", 8080, "server listen port")
 	flag.IntVar(&cfg.Fee, "fee", 10000, "transaction fee in satoish")
-	flag.StringVar(&cfg.DataDir, "dataDir", filepath.Join(homeDir, ".skycoin-exchange"), "data directory")
+	flag.StringVar(&cfg.DataDir, "dataDir", ".skycoin-exchange", "data directory")
 	flag.StringVar(&cfg.WalletName, "wltName", "server.wlt", "server's wallet file name")
-	flag.StringVar(&cfg.Seed, "s", "seed", "wallet's seed")
+	flag.StringVar(&cfg.Seed, "s", "test", "wallet's seed")
 	flag.StringVar(&cfg.AcntName, "acntName", "account.data", "accounts file name")
 	flag.IntVar(&cfg.UtxoPoolSize, "poolsize", 1000, "utxo pool size")
 
