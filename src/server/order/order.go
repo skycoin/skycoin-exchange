553710350246240c4de577b8fd00f7473695c352
iketheadore <heaven.mao@gmail.com>
2016-07-28 23:07:09 +0800

    move byPriceThenTime from book.go to order.go.

--- src/server/order/order.go
+++ src/server/order/order.go
@@ -14,3 +14,24 @@ type Order struct {
 	Amount      uint64 // the amount of this order.
 	CreatedTime uint64 // created time of the order.
 }
+
+type byPriceThenTime []Order
+
+func (bp byPriceThenTime) Len() int {
+	return len(bp)
+}
+
+func (bp byPriceThenTime) Less(i, j int) bool {
+	a := bp[i]
+	b := bp[j]
+	if a.Price > b.Price {
+		return true
+	} else if a.Price == b.Price {
+		return a.CreatedTime > b.CreatedTime
+	}
+	return false
+}
+
+func (bp byPriceThenTime) Swap(i, j int) {
+	bp[i], bp[j] = bp[j], bp[i]
+}
