0da8a2aba37e564b1d11c3cf35c2df66ec6607df
iketheadore <heaven.mao@gmail.com>
2016-07-30 17:54:30 +0800

    order book and test.

--- src/server/order/book_test.go
+++ src/server/order/book_test.go
@@ -1,42 +1,267 @@
 package order
 
 import (
-	"fmt"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
-var BidOrderList = []Order{
-	Order{Price: 100, CreatedTime: 132424, Amount: 1},
-	Order{Price: 102, CreatedTime: 132425, Amount: 1},
-	Order{Price: 103, CreatedTime: 132428, Amount: 1},
-	Order{Price: 101, CreatedTime: 132429, Amount: 1},
-}
-
-var AskOrderList = []Order{
-	Order{Price: 100, CreatedTime: 132424, Amount: 1},
-	Order{Price: 102, CreatedTime: 132425, Amount: 1},
-	Order{Price: 101, CreatedTime: 132429, Amount: 1},
-	Order{Price: 103, CreatedTime: 132428, Amount: 1},
-	Order{Price: 103, CreatedTime: 132438, Amount: 1},
-}
-
 func TestBook(t *testing.T) {
-	bk := Book{
-		askOrders: AskOrderList,
-		bidOrders: BidOrderList,
+	var BidOrderList = []Order{
+		Order{Price: 100, CreatedTime: 132424, Amount: 1},
+		Order{Price: 102, CreatedTime: 132425, Amount: 1},
+		Order{Price: 103, CreatedTime: 132428, Amount: 1},
+		Order{Price: 101, CreatedTime: 132429, Amount: 1},
+		Order{Price: 103, CreatedTime: 132430, Amount: 1},
 	}
 
-	bk.Sort()
+	var AskOrderList = []Order{
+		Order{Price: 100, CreatedTime: 132424, Amount: 1},
+		Order{Price: 102, CreatedTime: 132425, Amount: 1},
+		Order{Price: 101, CreatedTime: 132429, Amount: 1},
+		Order{Price: 103, CreatedTime: 132428, Amount: 1},
+		Order{Price: 103, CreatedTime: 132438, Amount: 1},
+	}
+	bk := Book{}
+
+	for _, bid := range BidOrderList {
+		bk.AddBid(bid)
+	}
+
+	for _, ask := range AskOrderList {
+		bk.AddAsk(ask)
+	}
+
 	if bk.bidOrders[0].Price < bk.bidOrders[1].Price {
-		t.Fail()
+		t.Fatal("bid price not sorted")
 	}
 
-	fmt.Println("asks")
-	if bk.askOrders[0].Price != bk.askOrders[1].Price {
-		t.Fail()
+	if bk.askOrders[0].Price > bk.askOrders[1].Price {
+		t.Fatal("ask price not sorted")
 	}
 
-	if bk.askOrders[0].CreatedTime < bk.askOrders[1].CreatedTime {
-		t.Fail()
+	if bk.askOrders[3].CreatedTime < bk.askOrders[4].CreatedTime {
+		t.Fatal("ask create time not sorted")
 	}
+}
+
+func TestMatch(t *testing.T) {
+	var BidOrderList = []Order{
+		Order{Type: Bid, Price: 100, CreatedTime: 132424, Amount: 1, RestAmt: 1},
+		Order{Type: Bid, Price: 102, CreatedTime: 132425, Amount: 1, RestAmt: 1},
+		Order{Type: Bid, Price: 103, CreatedTime: 132428, Amount: 1, RestAmt: 1},
+		Order{Type: Bid, Price: 101, CreatedTime: 132429, Amount: 1, RestAmt: 1},
+		Order{Type: Bid, Price: 103, CreatedTime: 132430, Amount: 1, RestAmt: 1},
+	}
+
+	var AskOrderList = []Order{
+		Order{Type: Ask, Price: 100, CreatedTime: 132424, Amount: 1, RestAmt: 1},
+		Order{Type: Ask, Price: 102, CreatedTime: 132425, Amount: 1, RestAmt: 1},
+		Order{Type: Ask, Price: 101, CreatedTime: 132429, Amount: 1, RestAmt: 1},
+		Order{Type: Ask, Price: 103, CreatedTime: 132428, Amount: 1, RestAmt: 1},
+		Order{Type: Ask, Price: 103, CreatedTime: 132438, Amount: 1, RestAmt: 1},
+	}
+
+	bk := Book{}
+	for _, bid := range BidOrderList {
+		bk.AddBid(bid)
+	}
+
+	for _, ask := range AskOrderList {
+		bk.AddAsk(ask)
+	}
+
+	ods := bk.Match()
+	// for _, od := range ods {
+	// 	fmt.Printf("type:%v, price:%d, amount:%d\n", od.Type, od.Price, od.Amount)
+	// }
+	// fmt.Println("len(ods):", len(ods))
+	assert.Equal(t, len(ods), 6)
+}
+
+// none match
+func TestNoneMatch(t *testing.T) {
+	var BidOrderList = []Order{
+		Order{Type: Bid, Price: 101, CreatedTime: 132429, Amount: 1, RestAmt: 1},
+		Order{Type: Bid, Price: 103, CreatedTime: 132430, Amount: 3, RestAmt: 3},
+	}
+
+	var AskOrderList = []Order{
+		Order{Type: Ask, Price: 104, CreatedTime: 132438, Amount: 1, RestAmt: 1},
+	}
+
+	bk := Book{}
+	for _, bid := range BidOrderList {
+		bk.AddBid(bid)
+	}
+
+	for _, ask := range AskOrderList {
+		bk.AddAsk(ask)
+	}
+
+	ods := bk.Match()
+	assert.Equal(t, len(ods), 0)
+}
+
+// zero bid n asks match.
+func TestMatchZero2N(t *testing.T) {
+	var BidOrderList = []Order{
+		Order{Type: Bid, Price: 100, CreatedTime: 132424, Amount: 1, RestAmt: 1},
+		Order{Type: Bid, Price: 102, CreatedTime: 132425, Amount: 1, RestAmt: 1},
+		Order{Type: Bid, Price: 103, CreatedTime: 132428, Amount: 1, RestAmt: 1},
+		Order{Type: Bid, Price: 101, CreatedTime: 132429, Amount: 1, RestAmt: 1},
+		Order{Type: Bid, Price: 103, CreatedTime: 132430, Amount: 7, RestAmt: 7},
+	}
+
+	var AskOrderList = []Order{
+		Order{Type: Ask, Price: 100, CreatedTime: 132424, Amount: 1, RestAmt: 1},
+		Order{Type: Ask, Price: 102, CreatedTime: 132425, Amount: 1, RestAmt: 1},
+		Order{Type: Ask, Price: 101, CreatedTime: 132429, Amount: 1, RestAmt: 1},
+		Order{Type: Ask, Price: 103, CreatedTime: 132428, Amount: 1, RestAmt: 1},
+		Order{Type: Ask, Price: 103, CreatedTime: 132438, Amount: 1, RestAmt: 1},
+	}
+
+	bk := Book{}
+	for _, bid := range BidOrderList {
+		bk.AddBid(bid)
+	}
+
+	for _, ask := range AskOrderList {
+		bk.AddAsk(ask)
+	}
+
+	ods := bk.Match()
+	// for _, od := range ods {
+	// 	fmt.Printf("type:%v, price:%d, amount:%d\n", od.Type, od.Price, od.Amount)
+	// }
+	assert.Equal(t, len(ods), 5)
+}
+
+// one bid match n asks.
+func TestMatchOne2N(t *testing.T) {
+	var BidOrderList = []Order{
+		Order{Type: Bid, Price: 100, CreatedTime: 132424, Amount: 1, RestAmt: 1},
+		Order{Type: Bid, Price: 102, CreatedTime: 132425, Amount: 1, RestAmt: 1},
+		Order{Type: Bid, Price: 103, CreatedTime: 132428, Amount: 1, RestAmt: 1},
+		Order{Type: Bid, Price: 101, CreatedTime: 132429, Amount: 1, RestAmt: 1},
+		Order{Type: Bid, Price: 103, CreatedTime: 132430, Amount: 3, RestAmt: 3},
+	}
+
+	var AskOrderList = []Order{
+		Order{Type: Ask, Price: 100, CreatedTime: 132424, Amount: 1, RestAmt: 1},
+		Order{Type: Ask, Price: 102, CreatedTime: 132425, Amount: 1, RestAmt: 1},
+		Order{Type: Ask, Price: 101, CreatedTime: 132429, Amount: 1, RestAmt: 1},
+		Order{Type: Ask, Price: 103, CreatedTime: 132428, Amount: 1, RestAmt: 1},
+		Order{Type: Ask, Price: 103, CreatedTime: 132438, Amount: 1, RestAmt: 1},
+	}
+
+	bk := Book{}
+	for _, bid := range BidOrderList {
+		bk.AddBid(bid)
+	}
+
+	for _, ask := range AskOrderList {
+		bk.AddAsk(ask)
+	}
+
+	ods := bk.Match()
+	// for _, od := range ods {
+	// 	fmt.Printf("type:%v, price:%d, amount:%d\n", od.Type, od.Price, od.Amount)
+	// }
+	// fmt.Println("len(ods):", len(ods))
+	assert.Equal(t, len(ods), 6)
+}
+
+// n bid match one asks.
+func TestMatchN2One(t *testing.T) {
+	var BidOrderList = []Order{
+		Order{Type: Bid, Price: 100, CreatedTime: 132424, Amount: 1, RestAmt: 1},
+		Order{Type: Bid, Price: 102, CreatedTime: 132425, Amount: 1, RestAmt: 1},
+		Order{Type: Bid, Price: 103, CreatedTime: 132428, Amount: 1, RestAmt: 1},
+		Order{Type: Bid, Price: 101, CreatedTime: 132429, Amount: 1, RestAmt: 1},
+		Order{Type: Bid, Price: 103, CreatedTime: 132430, Amount: 1, RestAmt: 1},
+	}
+
+	var AskOrderList = []Order{
+		Order{Type: Ask, Price: 100, CreatedTime: 132424, Amount: 4, RestAmt: 4},
+		Order{Type: Ask, Price: 102, CreatedTime: 132425, Amount: 1, RestAmt: 1},
+		Order{Type: Ask, Price: 101, CreatedTime: 132429, Amount: 1, RestAmt: 1},
+		Order{Type: Ask, Price: 103, CreatedTime: 132428, Amount: 1, RestAmt: 1},
+		Order{Type: Ask, Price: 103, CreatedTime: 132438, Amount: 1, RestAmt: 1},
+	}
+
+	bk := Book{}
+	for _, bid := range BidOrderList {
+		bk.AddBid(bid)
+	}
+
+	for _, ask := range AskOrderList {
+		bk.AddAsk(ask)
+	}
+
+	ods := bk.Match()
+	// for _, od := range ods {
+	// 	fmt.Printf("type:%v, price:%d, amount:%d\n", od.Type, od.Price, od.Amount)
+	// }
+	// fmt.Println("len(ods):", len(ods))
+	assert.Equal(t, len(ods), 5)
+}
+
+// n bid match n asks.
+func TestMatchN2N(t *testing.T) {
+	var BidOrderList = []Order{
+		Order{Type: Bid, Price: 100, CreatedTime: 132424, Amount: 1, RestAmt: 1},
+		Order{Type: Bid, Price: 102, CreatedTime: 132425, Amount: 2, RestAmt: 2},
+		Order{Type: Bid, Price: 103, CreatedTime: 132428, Amount: 1, RestAmt: 1},
+		Order{Type: Bid, Price: 101, CreatedTime: 132429, Amount: 1, RestAmt: 1},
+		Order{Type: Bid, Price: 103, CreatedTime: 132430, Amount: 1, RestAmt: 1},
+	}
+
+	var AskOrderList = []Order{
+		Order{Type: Ask, Price: 100, CreatedTime: 132424, Amount: 2, RestAmt: 2},
+		Order{Type: Ask, Price: 102, CreatedTime: 132425, Amount: 1, RestAmt: 1},
+		Order{Type: Ask, Price: 102, CreatedTime: 132440, Amount: 1, RestAmt: 1},
+		Order{Type: Ask, Price: 101, CreatedTime: 132429, Amount: 1, RestAmt: 1},
+		Order{Type: Ask, Price: 103, CreatedTime: 132428, Amount: 1, RestAmt: 1},
+		Order{Type: Ask, Price: 103, CreatedTime: 132438, Amount: 1, RestAmt: 1},
+	}
+
+	bk := Book{}
+	for _, bid := range BidOrderList {
+		bk.AddBid(bid)
+	}
+
+	for _, ask := range AskOrderList {
+		bk.AddAsk(ask)
+	}
+
+	ods := bk.Match()
+	// for _, od := range ods {
+	// 	fmt.Printf("type:%v, price:%d, amount:%d\n", od.Type, od.Price, od.Amount)
+	// }
+	// fmt.Println("len(ods):", len(ods))
+	assert.Equal(t, len(ods), 6)
+}
+
+// zero bid and ask
+func TestMatchZero(t *testing.T) {
+	var BidOrderList = []Order{}
+
+	var AskOrderList = []Order{}
+
+	bk := Book{}
+	for _, bid := range BidOrderList {
+		bk.AddBid(bid)
+	}
+
+	for _, ask := range AskOrderList {
+		bk.AddAsk(ask)
+	}
+
+	ods := bk.Match()
+	// for _, od := range ods {
+	// 	fmt.Printf("type:%v, price:%d, amount:%d\n", od.Type, od.Price, od.Amount)
+	// }
+	// fmt.Println("len(ods):", len(ods))
+	assert.Equal(t, len(ods), 0)
 }
--- src/server/order/order.go
+++ src/server/order/order.go
@@ -1,5 +1,7 @@
 package order
 
+import "github.com/skycoin/skycoin-exchange/src/server/account"
+
 type Type uint8
 
 const (
@@ -9,19 +11,22 @@ const (
 
 type Order struct {
 	ID          uint64 // order id.
+	AccountId   account.AccountID
 	Type        Type   // order type.
 	Price       uint64 // price of this order.
-	Amount      uint64 // the amount of this order.
+	Amount      uint64 // total amount of this order.
+	RestAmt     uint64 // rest amount.
 	CreatedTime uint64 // created time of the order.
 }
 
-type byPriceThenTime []Order
+type byPriceThenTimeDesc []Order
+type byPriceThenTimeAsc []Order
 
-func (bp byPriceThenTime) Len() int {
+func (bp byPriceThenTimeDesc) Len() int {
 	return len(bp)
 }
 
-func (bp byPriceThenTime) Less(i, j int) bool {
+func (bp byPriceThenTimeDesc) Less(i, j int) bool {
 	a := bp[i]
 	b := bp[j]
 	if a.Price > b.Price {
@@ -32,6 +37,25 @@ func (bp byPriceThenTime) Less(i, j int) bool {
 	return false
 }
 
-func (bp byPriceThenTime) Swap(i, j int) {
+func (bp byPriceThenTimeDesc) Swap(i, j int) {
+	bp[i], bp[j] = bp[j], bp[i]
+}
+
+func (bp byPriceThenTimeAsc) Len() int {
+	return len(bp)
+}
+
+func (bp byPriceThenTimeAsc) Less(i, j int) bool {
+	a := bp[i]
+	b := bp[j]
+	if a.Price < b.Price {
+		return true
+	} else if a.Price == b.Price {
+		return a.CreatedTime > b.CreatedTime
+	}
+	return false
+}
+
+func (bp byPriceThenTimeAsc) Swap(i, j int) {
 	bp[i], bp[j] = bp[j], bp[i]
 }
