bed5b5ba32e3b5d058ba44296907a6a7634a002d
iketheadore <heaven.mao@gmail.com>
2016-08-02 16:55:32 +0800

    check balance before handling orders.

--- src/server/api/order.go
+++ src/server/api/order.go
@@ -2,22 +2,24 @@ package api
 
 import (
 	"encoding/hex"
+	"errors"
 	"fmt"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 	"github.com/golang/glog"
 	"github.com/skycoin/skycoin-exchange/src/pp"
+	"github.com/skycoin/skycoin-exchange/src/server/account"
 	"github.com/skycoin/skycoin-exchange/src/server/engine"
 	"github.com/skycoin/skycoin-exchange/src/server/order"
+	"github.com/skycoin/skycoin-exchange/src/server/wallet"
 )
 
 func BidOrder(egn engine.Exchange) gin.HandlerFunc {
-	// TODO: check the balance.
 	return addOrder(order.Bid, egn)
 }
 
 func AskOrder(egn engine.Exchange) gin.HandlerFunc {
-	// TODO: check the balance.
 	return addOrder(order.Ask, egn)
 }
 
@@ -74,6 +76,33 @@ func addOrder(tp order.Type, egn engine.Exchange) gin.HandlerFunc {
 				break
 			}
 			aid := hex.EncodeToString(req.GetAccountId())
+			// find the account
+			pk := pp.BytesToPubKey(req.GetAccountId())
+			acnt, err := egn.GetAccount(account.AccountID(pk))
+			if err != nil {
+				rlt = pp.MakeErrResWithCode(pp.ErrCode_WrongAccountId)
+				break
+			}
+
+			ct, bal, err := needBalance(tp, req)
+			if err != nil {
+				rlt = pp.MakeErrResWithCode(pp.ErrCode_WrongRequest)
+				break
+			}
+
+			if acnt.GetBalance(ct) < bal {
+				rlt = pp.MakeErrRes(fmt.Errorf("%s balance is not sufficient", ct))
+				break
+			}
+
+			if tp == order.Bid {
+				// decrease the balance, in case of double use the coins.
+				if err := acnt.DecreaseBalance(ct, bal); err != nil {
+					rlt = pp.MakeErrRes(err)
+					break
+				}
+			}
+
 			odr := order.New(aid, tp, req.GetPrice(), req.GetAmount())
 			oid, err := egn.AddOrder(req.GetCoinPair(), *odr)
 			if err != nil {
@@ -91,5 +120,30 @@ func addOrder(tp order.Type, egn engine.Exchange) gin.HandlerFunc {
 			return
 		}
 		c.JSON(200, *rlt)
+	}
+}
+
+func needBalance(tp order.Type, req *pp.OrderReq) (wallet.CoinType, uint64, error) {
+	pair := strings.Split(req.GetCoinPair(), "/")
+	if len(pair) != 2 {
+		return -1, 0, errors.New("error coin pair")
+	}
+
+	mainCt, err := wallet.ConvertCoinType(pair[0])
+	if err != nil {
+		return -1, 0, err
+	}
+	subCt, err := wallet.ConvertCoinType(pair[1])
+	if err != nil {
+		return -1, 0, err
+	}
+
+	switch tp {
+	case order.Bid:
+		return subCt, req.GetPrice() * req.GetAmount(), nil
+	case order.Ask:
+		return mainCt, req.GetAmount(), nil
+	default:
+		return -1, 0, errors.New("unknow order type")
 	}
 }
