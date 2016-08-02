26092d6050f0fabfe07f6d07e4a8fce9226f1a42
iketheadore <heaven.mao@gmail.com>
2016-08-02 14:28:12 +0800

    order api add GetOrders func.

--- src/server/api/order.go
+++ src/server/api/order.go
@@ -2,6 +2,7 @@ package api
 
 import (
 	"encoding/hex"
+	"fmt"
 
 	"github.com/gin-gonic/gin"
 	"github.com/golang/glog"
@@ -11,6 +12,59 @@ import (
 )
 
 func BidOrder(egn engine.Exchange) gin.HandlerFunc {
+	// TODO: check the balance.
+	return addOrder(order.Bid, egn)
+}
+
+func AskOrder(egn engine.Exchange) gin.HandlerFunc {
+	// TODO: check the balance.
+	return addOrder(order.Ask, egn)
+}
+
+func GetOrders(egn engine.Exchange) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		// get start and end.
+		rlt := &pp.EmptyRes{}
+		for {
+			req := pp.GetOrderReq{}
+			if err := c.BindJSON(&req); err != nil {
+				rlt = pp.MakeErrResWithCode(pp.ErrCode_WrongRequest)
+				break
+			}
+			tp := order.MustTypeFromStr(req.GetType())
+			ords, err := egn.GetOrders(req.GetCoinPair(), tp, req.GetStart(), req.GetEnd())
+			if err != nil {
+				rlt = pp.MakeErrResWithCode(pp.ErrCode_WrongRequest)
+				break
+			}
+			res := pp.GetOrderRes{
+				CoinPair: req.CoinPair,
+				Type:     req.Type,
+				Orders:   make([]*pp.Order, len(ords)),
+			}
+
+			for i := range ords {
+				res.Orders[i] = &pp.Order{
+					AccountId:   &ords[i].AccountID,
+					Id:          &ords[i].ID,
+					Type:        req.Type,
+					Price:       &ords[i].Price,
+					Amount:      &ords[i].Amount,
+					RestAmt:     &ords[i].RestAmt,
+					CreatedTime: &ords[i].CreatedTime,
+				}
+			}
+
+			res.Result = pp.MakeResultWithCode(pp.ErrCode_Success)
+			c.JSON(200, res)
+			return
+		}
+
+		c.JSON(200, rlt)
+	}
+}
+
+func addOrder(tp order.Type, egn engine.Exchange) gin.HandlerFunc {
 	return func(c *gin.Context) {
 		rlt := &pp.EmptyRes{}
 		req := &pp.OrderReq{}
@@ -20,7 +74,7 @@ func BidOrder(egn engine.Exchange) gin.HandlerFunc {
 				break
 			}
 			aid := hex.EncodeToString(req.GetAccountId())
-			odr := order.New(aid, order.Bid, req.GetPrice(), req.GetAmount())
+			odr := order.New(aid, tp, req.GetPrice(), req.GetAmount())
 			oid, err := egn.AddOrder(req.GetCoinPair(), *odr)
 			if err != nil {
 				glog.Error(err)
@@ -28,7 +82,7 @@ func BidOrder(egn engine.Exchange) gin.HandlerFunc {
 				break
 			}
 
-			glog.Info("new order:%d", oid)
+			glog.Info(fmt.Sprintf("new %s order:%d", tp, oid))
 			res := pp.OrderRes{
 				AccountId: req.AccountId,
 				OrderId:   &oid,
@@ -36,19 +90,6 @@ func BidOrder(egn engine.Exchange) gin.HandlerFunc {
 			reply(c, res)
 			return
 		}
-
 		c.JSON(200, *rlt)
-	}
-}
-
-func AskOrder(egn engine.Exchange) gin.HandlerFunc {
-	return func(c *gin.Context) {
-
-	}
-}
-
-func GetOrderbook(egn engine.Exchange) gin.HandlerFunc {
-	return func(c *gin.Context) {
-
 	}
 }
