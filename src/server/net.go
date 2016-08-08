05311667b3396ed758e89a54c1764a6d9341f513
iketheadore <heaven.mao@gmail.com>
2016-08-08 21:48:20 +0800

    complete the server router info.

--- src/server/net.go
+++ src/server/net.go
@@ -8,7 +8,15 @@ import (
 
 func NewNet(ee engine.Exchange, quit chan bool) *net.Engine {
 	nt := net.New(quit)
-	nt.Use(api.Authorize(ee))
+	auth := nt.Group("/auth", api.Authorize(ee))
+	{
+		auth.Register("/create/account", api.CreateAccount(ee))
+		auth.Register("/create/deposit_address", api.GetNewAddress(ee))
+		auth.Register("/get/balance", api.GetBalance(ee))
+		auth.Register("/withdrawl", api.Withdraw(ee))
+		auth.Register("/create/order", api.CreateOrder(ee))
+	}
 	nt.Register("/get/coins", api.GetCoins(ee))
+	nt.Register("/get/orders", api.GetOrders(ee))
 	return nt
 }
