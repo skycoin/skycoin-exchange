161fe43aaa981ce82a3eebe1969474e1df643eee
iketheadore <heaven.mao@gmail.com>
2016-08-08 12:11:23 +0800

    net request helper, for decoding data.

--- src/server/net/request.go
+++ src/server/net/request.go
@@ -23,13 +23,13 @@ func (req *Request) Read(r io.Reader) error {
 	if err := binary.Read(r, binary.BigEndian, &len); err != nil {
 		return err
 	}
-
 	req.Raw = make([]byte, len)
 	if err := binary.Read(r, binary.BigEndian, &req.Raw); err != nil {
 		return err
 	}
 
 	if err := json.Unmarshal(req.Raw[:], &req.Request); err != nil {
+		logger.Error("%s", err)
 		return err
 	}
 	return nil
