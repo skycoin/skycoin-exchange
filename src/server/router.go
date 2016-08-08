db1e2e3bed11771345ad23f720167c9f4500136f
iketheadore <heaven.mao@gmail.com>
2016-08-08 12:09:20 +0800

    comment gin router engine.

--- src/server/router.go
+++ src/server/router.go
@@ -1,29 +1,23 @@
 package server
 
-import (
-	"github.com/gin-gonic/gin"
-	"github.com/skycoin/skycoin-exchange/src/server/api"
-	"github.com/skycoin/skycoin-exchange/src/server/engine"
-)
-
-func NewRouter(ee engine.Exchange) *gin.Engine {
-	r := gin.New()
-	r.Use(Logger())
-	r.Use(gin.Recovery())
-
-	// the Authorize middle will decrypt the request, and encrypt the response.
-	v1 := r.Group("/api/v1")
-	authReq := v1.Group("/", api.Authorize(ee))
-	{
-		authReq.POST("/accounts", api.CreateAccount(ee)) // create account
-		authReq.POST("/deposit_address", api.GetNewAddress(ee))
-		authReq.POST("/account/withdrawal", api.Withdraw(ee))
-		authReq.POST("/account/balance", api.GetBalance(ee))
-
-		authReq.POST("/account/order/:type", api.CreateOrder(ee))
-	}
-
-	v1.POST("/orders/:type", api.GetOrders(ee))
-	// v1.GET("/coins", api.GetCoins(ee))
-	return r
-}
+// func NewRouter(ee engine.Exchange) *gin.Engine {
+// 	r := gin.New()
+// 	r.Use(Logger())
+// 	r.Use(gin.Recovery())
+//
+// 	// the Authorize middle will decrypt the request, and encrypt the response.
+// 	v1 := r.Group("/api/v1")
+// 	authReq := v1.Group("/", api.Authorize(ee))
+// 	{
+// 		authReq.POST("/accounts", api.CreateAccount(ee)) // create account
+// 		authReq.POST("/deposit_address", api.GetNewAddress(ee))
+// 		authReq.POST("/account/withdrawal", api.Withdraw(ee))
+// 		authReq.POST("/account/balance", api.GetBalance(ee))
+//
+// 		authReq.POST("/account/order/:type", api.CreateOrder(ee))
+// 	}
+//
+// 	v1.POST("/orders/:type", api.GetOrders(ee))
+// 	// v1.GET("/coins", api.GetCoins(ee))
+// 	return r
+// }
