365ed9e28428bc687b2b6d26e011f73a30c14323
iketheadore <heaven.mao@gmail.com>
2016-08-11 20:44:01 +0800

    change group filed midhandlers to preHandlers and handlers to regHandlers.

--- src/server/net/group.go
+++ src/server/net/group.go
@@ -5,15 +5,18 @@ import (
 	"path/filepath"
 )
 
+// Group maintains a group of handlers of specific prefix path,
 type Group struct {
-	Path        string
-	midHandlers []HandlerFunc
-	handlers    map[string]HandlerFunc
+	Path        string                 // group path.
+	preHandlers []HandlerFunc          // handlers must be executed before registed handlers.
+	regHandlers map[string]HandlerFunc // registed handlers.
 }
 
+// Register register path and handlers into this group.
+// the real path will be prefixed with group path.
 func (gp *Group) Register(path string, handler HandlerFunc) {
-	if _, ok := gp.handlers[path]; ok {
+	if _, ok := gp.regHandlers[path]; ok {
 		panic(fmt.Sprintf("dubplicate path:%s in group:%s", path, gp.Path))
 	}
-	gp.handlers[filepath.Join(gp.Path, path)] = handler
+	gp.regHandlers[filepath.Join(gp.Path, path)] = handler
 }
