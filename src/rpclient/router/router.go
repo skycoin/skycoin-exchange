adc902a670c185e34f3e4d43250035128e0775f9
iketheadore <heaven.mao@gmail.com>
2016-08-16 14:40:21 +0800

    client adjust the router, and add api for querying utxos.

--- src/rpclient/router/router.go
+++ src/rpclient/router/router.go
@@ -8,17 +8,29 @@ import (
 
 func New(se api.Servicer) *http.ServeMux {
 	mux := http.NewServeMux()
-	// base handlers.
+	registerBaseHandlers(mux, se)
+	registerOrderHandlers(mux, se)
+	registerUtxoHandlers(mux, se)
+	return mux
+}
+
+// base handlers.
+func registerBaseHandlers(mux *http.ServeMux, se api.Servicer) {
 	mux.Handle("/api/v1/coins", api.GetCoins(se))
 	mux.Handle("/api/v1/accounts", api.CreateAccount(se))
 	mux.Handle("/api/v1/account/deposit_address", api.GetNewAddress(se))
 	mux.Handle("/api/v1/account/balance", api.GetBalance(se))
 	mux.Handle("/api/v1/account/withdrawal", api.Withdraw(se))
+}
 
-	// order handlers
+// order handlers
+func registerOrderHandlers(mux *http.ServeMux, se api.Servicer) {
 	mux.Handle("/api/v1/account/order", api.CreateOrder(se))
 	mux.Handle("/api/v1/orders/bid", api.GetBidOrders(se))
 	mux.Handle("/api/v1/orders/ask", api.GetAskOrders(se))
+}
 
-	return mux
+// utxos handlers
+func registerUtxoHandlers(mux *http.ServeMux, se api.Servicer) {
+	mux.Handle("/api/v1/utxos", api.GetUtxos(se))
 }
