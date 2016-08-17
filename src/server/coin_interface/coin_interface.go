73be2cc145e067bd8f1f25159ff899f2243f70a7
iketheadore <heaven.mao@gmail.com>
2016-08-17 22:53:22 +0800

    define gateway map in coin_interface.

--- src/server/coin_interface/coin_interface.go
+++ src/server/coin_interface/coin_interface.go
@@ -1,7 +1,50 @@
 package coin_interface
 
+import (
+	"fmt"
+	"io"
+
+	"github.com/skycoin/skycoin-exchange/src/pp"
+	"github.com/skycoin/skycoin-exchange/src/server/wallet"
+)
+
 type AddressEntry struct {
 	Address string
 	Public  string
 	Secret  string
+}
+
+// CoinHandlers records the handlers for different coin.
+var gateways = map[wallet.CoinType]Gateway{}
+
+// Transaction tx interface
+type Transaction interface {
+	Serialize() (string, error)
+	Deserialize(r io.Reader) error
+	ToPPTx() *pp.Tx // translate to *pp.Tx
+}
+
+// CoinHandler interface for handlering all coin relevance things.
+type Gateway interface {
+	TxHandler
+}
+
+type TxHandler interface {
+	GetTx(txid string) (Transaction, error)
+	GetRawTx(txid string) (string, error)
+	DecodeRawTx(rawtx string) (Transaction, error)
+}
+
+func RegisterGateway(tp wallet.CoinType, gw Gateway) {
+	if _, ok := gateways[tp]; ok {
+		panic(fmt.Errorf("%s gateway already registered"))
+	}
+	coinHandlers[tp] = gw
+}
+
+func GetGateway(tp wallet.CoinType) (Gateway, error) {
+	if c, ok := gateways[tp]; ok {
+		return c, nil
+	}
+	return nil, fmt.Errorf("%s handler not registerd")
 }
