d6e1db2427f5f387027143e2b1625d2454180846
iketheadore <heaven.mao@gmail.com>
2016-08-19 21:00:56 +0800

    adjust coin.go

--- src/server/coin/coin.go
+++ src/server/coin/coin.go
@@ -9,6 +9,35 @@ import (
 // gateways records the handlers for different coin.
 var gateways = map[Type]Gateway{}
 
+// Gateway coin gateway, once a coin implemented this interface,
+// then this coin can be registered in this exchange system.
+type Gateway interface {
+	TxHandler
+}
+
+// TxHandler transaction handlers interface for gateway.
+type TxHandler interface {
+	GetTx(txid string) (*pp.Tx, error)
+	GetRawTx(txid string) (string, error)
+	InjectTx(rawtx string) (string, error)
+}
+
+// RegisterGateway register gateway for specific coin.
+func RegisterGateway(tp Type, gw Gateway) {
+	if _, ok := gateways[tp]; ok {
+		panic(fmt.Errorf("%s gateway already registered"))
+	}
+	gateways[tp] = gw
+}
+
+// GetGateway get gateway of specific coin.
+func GetGateway(tp Type) (Gateway, error) {
+	if c, ok := gateways[tp]; ok {
+		return c, nil
+	}
+	return nil, fmt.Errorf("%s handler not registerd")
+}
+
 type AddressEntry struct {
 	Address string
 	Public  string
@@ -53,39 +82,4 @@ func TypeFromStr(ct string) (Type, error) {
 	default:
 		return -1, fmt.Errorf("unknow coin type:%s", ct)
 	}
-}
-
-// Transaction tx interface
-type Transaction interface {
-	Bytes() ([]byte, error)
-	ToPPTx() *pp.Tx
-}
-
-// Gateway interface for handlering all coin relevance things.
-type Gateway interface {
-	TxHandler
-}
-
-// TxHandler transaction handlers interface for gateway.
-type TxHandler interface {
-	GetTx(txid string) (*pp.Tx, error)
-	GetRawTx(txid string) (string, error)
-	// DecodeRawTx(r io.Reader) (Transaction, error)
-	InjectTx(rawtx string) (string, error)
-}
-
-// RegisterGateway register gateway for specific coin.
-func RegisterGateway(tp Type, gw Gateway) {
-	if _, ok := gateways[tp]; ok {
-		panic(fmt.Errorf("%s gateway already registered"))
-	}
-	gateways[tp] = gw
-}
-
-// GetGateway get gateway of specific coin.
-func GetGateway(tp Type) (Gateway, error) {
-	if c, ok := gateways[tp]; ok {
-		return c, nil
-	}
-	return nil, fmt.Errorf("%s handler not registerd")
 }
