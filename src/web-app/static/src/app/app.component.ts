4887f0ada1c1a149f1c5dfd98a227f253332b85a
Li Xing <polarislee1984@outlook.com>
2016-08-24 23:33:26 +0800

    commit with retrieve orders/balances

--- src/web-app/static/src/app/app.component.ts
+++ src/web-app/static/src/app/app.component.ts
@@ -15,6 +15,9 @@ import 'rxjs/add/operator/catch';
 export class AppComponent implements OnInit {
     bidList: Array<any>;
     askList: Array<any>;
+    accountId:any;
+    key:any;
+    balance:any;
 
     //Constructor method for load HTTP object
     constructor(private http: Http) { }
@@ -22,18 +25,55 @@ export class AppComponent implements OnInit {
     ngOnInit() {
         this.bidList = [];
         this.askList = [];
-        this.loadBidList();
-        this.loadAskList();
+        this.accountId = null;
+        this.key = null;
+        this.balance = {
+          skycoin:0,
+          bitcoin:0
+        };
+        var headers = new Headers();
+        headers.append('Content-Type', 'application/x-www-form-urlencoded');
+        var self = this;
+        this.http.post('/api/v1/accounts', '')
+            .map((res) => res.json())
+            .subscribe(data => {
+              console.log('request account', data);
+              if (data.result.success) {
+                self.accountId = data.account_id;
+                self.key = data.key;
+                self.loadBidList();
+                self.loadAskList();
+                self.getBalance();
+                /*
+                this.http.post('/api/v1/account/deposit_address?id=self.accountId&key=self.key&coin_type=skycoin', '')
+                    .map((res) => res.json())
+                    .subscribe(data => {
+                    console.log("deposite", data);
+                    self.loadBidList();
+                    self.loadAskList();
+                    }, err => console.log("Error on load outputs: " + err), () => console.log('Connection load done'));
+                    */
+              } else {
+                alert("Cannot get account from server. please check connection with server");
+                return;
+              }
+            }, err => console.log("Error on load outputs: " + err), () => console.log('Connection load done'));
     }
 
     loadBidList() {
         var headers = new Headers();
         headers.append('Content-Type', 'application/x-www-form-urlencoded');
-        this.http.get('http://localhost:6060/api/v1/orders/bid?coin_pair=bitcoin/skycoin', { headers: headers })
+        var url = '/api/v1/orders/bid?coin_pair=bitcoin/skycoin&id=' + this.accountId + "&key=" + this.key + '&start=1&end=10';
+        this.http.get(url, { headers: headers })
             .map((res) => res.json())
             .subscribe(data => {
-            console.log(data);
-            this.bidList = data;
+              console.log("get bid list", url, data);
+              if (data.result.success) {
+                this.bidList = data.orders;
+              } else {
+                return;
+              }
+
             }, err => console.log("Error on load outputs: " + err), () => console.log('Connection load done'));
 
         //input data format
@@ -97,57 +137,35 @@ export class AppComponent implements OnInit {
     loadAskList() {
         var headers = new Headers();
         headers.append('Content-Type', 'application/x-www-form-urlencoded');
-        this.http.get('http://localhost:6060/api/v1/orders/ask', { headers: headers })
+        var url = '/api/v1/orders/ask?coin_pair=bitcoin/skycoin&id=' + this.accountId + '&key=' + this.key + '&start=1&end=10';
+        this.http.get(url, { headers: headers })
             .map((res) => res.json())
             .subscribe(data => {
-                console.log(data);
-                this.askList = data;
+              console.log("get ask list", url, data);
+              if (data.result.success) {
+                this.askList = data.orders;
+              } else {
+                return;
+              }
             }, err => console.log("Error on load outputs: " + err), () => console.log('Connection load done'));
+    }
 
-        this.askList.push({
-            "id": 8,
-            "type": "bid",
-            "price": 25,
-            "amount": 90000,
-            "rest_amt": 90000,
-            "created_at": 1470193222,
-            "coin_type":"SKY"
-        });
-        this.askList.push({
-            "id": 8,
-            "type": "bid",
-            "price": 25,
-            "amount": 90000,
-            "rest_amt": 90000,
-            "created_at": 1470293222,
-            "coin_type":"BTC"
-        });
-        this.askList.push({
-            "id": 8,
-            "type": "bid",
-            "price": 25,
-            "amount": 90000,
-            "rest_amt": 90000,
-            "created_at": 1470393222,
-            "coin_type":"SKY"
-        });
-        this.askList.push({
-            "id": 8,
-            "type": "bid",
-            "price": 25,
-            "amount": 90000,
-            "rest_amt": 90000,
-            "created_at": 1470493222,
-            "coin_type":"SKY"
-        });
-        this.askList.push({
-            "id": 8,
-            "type": "bid",
-            "price": 25,
-            "amount": 90000,
-            "rest_amt": 90000,
-            "created_at": 1470593222,
-            "coin_type":"BTC"
-        });
+    getBalance() {
+        var headers = new Headers();
+        headers.append('Content-Type', 'application/x-www-form-urlencoded');
+        var url = '/api/v1/account/balance?id=' + this.accountId + '&key=' + this.key + '&coin_type=skycoin';
+        this.http.get(url, { headers: headers })
+            .map((res) => res.json())
+            .subscribe(data => {
+              console.log("get skycoin balance", url, data);
+              this.balance.skycoin = data.balance;
+            }, err => console.log("Error on load outputs: " + err), () => console.log('Connection load done'));
+        url = '/api/v1/account/balance?id=' + this.accountId + '&key=' + this.key + '&coin_type=bitcoin';
+        this.http.get(url, { headers: headers })
+            .map((res) => res.json())
+            .subscribe(data => {
+              console.log("get bitcoin balance", url, data);
+              this.balance.bitcoin = data.balance;
+            }, err => console.log("Error on load outputs: " + err), () => console.log('Connection load done'));
     }
 }
