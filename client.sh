a3684ea6cd589646a24d56d4d3996aeee0aca7b7
iketheadore <heaven.mao@gmail.com>
2016-08-26 21:21:52 +0800

    update client.sh script

--- client.sh
+++ client.sh
@@ -1,1 +1,11 @@
-go build cmd/client/client.go&& ./client
+#!/usr/bin/env bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+echo "skycoin-exchange client binary dir:" "$DIR"
+
+pushd "$DIR" >/dev/null
+
+go run cmd/client/client.go --gui-dir="${DIR}/src/web-app/static/" $@
+
+popd >/dev/null
