2ebaf5a5632cf06b622c4a97bc3f2fe0d860e681
iketheadore <heaven.mao@gmail.com>
2016-11-04 13:59:57 +0800

    Add Send coin related interfaces

--- src/api/mobile/node.go
+++ src/api/mobile/node.go
@@ -1,6 +1,12 @@
 package mobile
 
+import "github.com/skycoin/skycoin-exchange/src/coin"
+import "github.com/skycoin/skycoin/src/cipher"
+
 type noder interface {
-	GetBalance(addr string) (uint64, error)
+	GetBalance(addrs []string) (uint64, error)
 	ValidateAddr(addr string) error
+	PrepareTx(addrs []string, toAddr string, amt uint64) ([]coin.TxIn, []string, interface{}, error)
+	CreateRawTx(txIns []coin.TxIn, keys []cipher.SecKey, txOuts interface{}) (string, error)
+	BroadcastTx(rawtx string) (string, error)
 }
