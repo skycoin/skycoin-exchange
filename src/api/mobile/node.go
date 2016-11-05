ecdbee2c4a73b1b9b387634a9e223b68a8ca8d5e
iketheadore <heaven.mao@gmail.com>
2016-11-05 09:22:47 +0800

    Adjust PrepareTx and CreateRawTx interface

--- src/api/mobile/node.go
+++ src/api/mobile/node.go
@@ -1,12 +1,11 @@
 package mobile
 
 import "github.com/skycoin/skycoin-exchange/src/coin"
-import "github.com/skycoin/skycoin/src/cipher"
 
 type noder interface {
 	GetBalance(addrs []string) (uint64, error)
 	ValidateAddr(addr string) error
-	PrepareTx(addrs []string, toAddr string, amt uint64) ([]coin.TxIn, []string, interface{}, error)
-	CreateRawTx(txIns []coin.TxIn, keys []cipher.SecKey, txOuts interface{}) (string, error)
+	PrepareTx(params interface{}) ([]coin.TxIn, interface{}, error)
+	CreateRawTx(txIns []coin.TxIn, getKey coin.GetPrivKey, txOuts interface{}) (string, error)
 	BroadcastTx(rawtx string) (string, error)
 }
