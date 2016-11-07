b1f67c2a81849ff78f5df07c92c3c7c034edfe6e
iketheadore <heaven.mao@gmail.com>
2016-11-07 18:09:22 +0800

    Remove gateway init

--- src/api/mobile/api.go
+++ src/api/mobile/api.go
@@ -7,7 +7,6 @@ import (
 	"strconv"
 
 	"github.com/skycoin/skycoin-exchange/src/coin"
-	skycoin "github.com/skycoin/skycoin-exchange/src/coin/skycoin"
 	"github.com/skycoin/skycoin-exchange/src/wallet"
 )
 
@@ -35,7 +34,6 @@ func Init(cfg *Config) {
 		"skycoin": &skyNode{NodeAddr: config.ServerAddr},
 		"bitcoin": &btcNode{NodeAddr: config.ServerAddr},
 	}
-	skycoin.ServeAddr = cfg.ServerAddr
 }
 
 // NewWallet create a new wallet base on the wallet type and seed
