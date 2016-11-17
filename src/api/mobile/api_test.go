becd8543ebe6e9f69c48ee085987a6c5659e4eca
iketheadore <heaven.mao@gmail.com>
2016-11-17 21:35:55 +0800

    Add test cases for api of GetTransactionID

--- src/api/mobile/api_test.go
+++ src/api/mobile/api_test.go
@@ -2,6 +2,7 @@ package mobile_test
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	"math/rand"
 	"os"
@@ -54,7 +55,7 @@ func TestGetBalance(t *testing.T) {
 		expect   uint64
 	}{
 		{"skycoin", "cBnu9sUvv12dovBmjQKTtfE4rbjMmf3fzW", 4000000},
-		{"bitcoin", "1EknG7EauSW4zxFtSrCQSHe5PJenkn55s6", 994000},
+		{"bitcoin", "1EknG7EauSW4zxFtSrCQSHe5PJenkn55s6", 938000},
 	}
 	for _, td := range testData {
 		b, err := api.GetBalance(td.coinType, td.address)
@@ -193,4 +194,92 @@ func TestSendSky(t *testing.T) {
 		t.Fatal(err)
 	}
 	fmt.Println(txid)
+}
+
+func TestGetTransactionByID(t *testing.T) {
+	type args struct {
+		coinType string
+		txid     string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		contain string
+		wantErr error
+	}{
+		// TODO: Add test cases.
+		{
+			"bitcoin normal",
+			args{
+				"bitcoin",
+				"69be3a3b98541e609f5a4935f94c92012d2b3e3437e9508770ba2257f532142f",
+			},
+			"0000000000000000021f3adb9ce12e3c70a42cfb6b7095805bee7bdefb392725",
+			nil,
+		},
+		{
+			"bitcoin invalid txid len",
+			args{
+				"bitcoin",
+				"69be3a3b98541e6",
+			},
+			"",
+			errors.New("invalid transaction id"),
+		},
+		{
+			"bitcoin invalid txid",
+			args{
+				"bitcoin",
+				"69be3a3b98541e609f5a4935f94c92012d2b3e3437e9508770ba2257f532142d",
+			},
+			"",
+			errors.New("not found"),
+		},
+		{
+			"skycoin normal",
+			args{
+				"skycoin",
+				"b1481d614ffcc27408fe2131198d9d2821c78601a0aa23d8e9965b2a5196edc0",
+			},
+			"7583587d02bedbeb3c15dde9e13baac36b0eb2b7ba7b2063c323a226d0784619",
+			nil,
+		},
+		{
+			"skycoin invalid txid len",
+			args{
+				"skycoin",
+				"b1481d",
+			},
+			"",
+			errors.New("invalid transaction id"),
+		},
+		{
+			"skycoin invalid txid",
+			args{
+				"skycoin",
+				"b1481d614ffcc27408fe2131198d9d2821c78601a0aa23d8e9965b2a5196edc1",
+			},
+			"",
+			errors.New("not found\n"),
+		},
+		{
+			"invalid coin type",
+			args{
+				"unknow",
+				"",
+			},
+			"",
+			errors.New("unknow is not supported"),
+		},
+	}
+	for _, tt := range tests {
+		got, err := api.GetTransactionByID(tt.args.coinType, tt.args.txid)
+		if !assert.Equal(t, err, tt.wantErr) {
+			t.Errorf("%q. GetTransactionByID() error = %v, wantErr %v", tt.name, err, tt.wantErr)
+			continue
+		}
+		if !strings.Contains(got, tt.contain) {
+			t.Errorf("%q. GetTransactionByID() = %v, want %v", tt.name, got, tt.contain)
+		}
+	}
 }
