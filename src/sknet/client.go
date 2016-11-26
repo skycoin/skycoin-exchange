7bf53766735a7d30ec609768fd90301b8afcbc30
iketheadore <heaven.mao@gmail.com>
2016-11-26 15:34:37 +0800

    Clean up code

--- src/sknet/client.go
+++ src/sknet/client.go
@@ -25,17 +25,6 @@ func Get(addr string, path string, v interface{}) (*Response, error) {
 	if err := Write(c, r); err != nil {
 		return nil, err
 	}
-	// d, err := json.Marshal(r)
-	// if err != nil {
-	// 	return nil, err
-	// }
-
-	// buf := make([]byte, 4+len(d))
-	// binary.BigEndian.PutUint32(buf[:], uint32(len(d)))
-	// copy(buf[4:], d)
-	// if err := binary.Write(c, binary.BigEndian, buf); err != nil {
-	// 	return nil, err
-	// }
 
 	rsp := Response{}
 	if err := Read(c, &rsp); err != nil {
