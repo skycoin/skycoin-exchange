1a966120201613aa219cd9a46078fbb91b0c43ca
iketheadore <heaven.mao@gmail.com>
2016-12-10 09:16:47 +0800

    Clean up the coin.go

--- src/coin/coin.go
+++ src/coin/coin.go
@@ -1,20 +1,13 @@
 package coin
 
-import (
-	"fmt"
-
-	"github.com/skycoin/skycoin-exchange/src/pp"
-)
-
-// gateways records the handlers for different coin.
-var gateways = map[Type]Gateway{}
+import "github.com/skycoin/skycoin-exchange/src/pp"
 
 // Gateway coin gateway, once a coin implemented this interface,
 // then this coin can be registered in this exchange system.
 type Gateway interface {
 	TxHandler
 	Symbol() string // return the coin symbol, SKY, BTC, MZC, etc.
-	Type() Type     // return the coin type, skycoin, bitcoin, etc.
+	Type() string   // return the coin type, skycoin, bitcoin, etc.
 	// GetBalance interface for getting balance, the return value is an interface{}, cause
 	// the balance struct of skycoin and bitcoin are not the same.
 	GetBalance(addrs []string) (pp.Balance, error)
@@ -42,64 +35,9 @@ type TxIn struct {
 // GetPrivKey is a callback func used for SignTx func to get relevant private key of specific address.
 type GetPrivKey func(addr string) (string, error)
 
-// RegisterGateway register gateway for specific coin.
-func RegisterGateway(tp Type, gw Gateway) {
-	if _, ok := gateways[tp]; ok {
-		panic(fmt.Errorf("%s gateway already registered", tp))
-	}
-	gateways[tp] = gw
-}
-
-// GetGateway get gateway of specific coin.
-func GetGateway(tp Type) (Gateway, error) {
-	if c, ok := gateways[tp]; ok {
-		return c, nil
-	}
-	return nil, fmt.Errorf("%s gateway not registerd", tp)
-}
-
+// AddressEntry represents the wallet address
 type AddressEntry struct {
 	Address string `json:"address"`
 	Public  string `json:"pubkey"`
 	Secret  string `json:"seckey"`
-}
-
-// Type represents the coin type.
-type Type int8
-
-const (
-	Bitcoin Type = iota
-	Skycoin
-	// Shellcoin
-	// Ethereum
-	// other coins...
-)
-
-var coinStr = []string{
-	Bitcoin: "bitcoin",
-	Skycoin: "skycoin",
-}
-
-func (c Type) String() string {
-	switch c {
-	case Bitcoin:
-		return coinStr[c]
-	case Skycoin:
-		return coinStr[c]
-	default:
-		// return fmt.Sprintf("unknow coin type:%d", c)
-		panic(fmt.Sprintf("unknow coin type:%d", c))
-	}
-}
-
-//TypeFromStr convert string to Type
-func TypeFromStr(ct string) (Type, error) {
-	switch ct {
-	case "bitcoin":
-		return Bitcoin, nil
-	case "skycoin":
-		return Skycoin, nil
-	default:
-		return -1, fmt.Errorf("unknow coin type:%s", ct)
-	}
 }
