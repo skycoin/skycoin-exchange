97b35efdca48c51d3698f03034e290f0eb9c1c86
iketheadore <heaven.mao@gmail.com>
2016-12-10 09:33:09 +0800

    Define mzcoin

---
+++ src/coin/mzcoin/mzcoin.go
@@ -0,0 +1,23 @@
+package mzcoin
+
+import (
+	skycoin "github.com/skycoin/skycoin-exchange/src/coin/skycoin"
+)
+
+// Type represents mzcoin coin type
+var Type = "mzcoin"
+
+// Mzcoin will implement coin.Gateway interface
+type Mzcoin struct {
+	skycoin.Skycoin // embeded from skycoin , as all apis are the same as skycoin
+}
+
+// Symbol returns the mzcoin symbol
+func (mz Mzcoin) Symbol() string {
+	return "MZC"
+}
+
+// Type returns mzcoin type
+func (mz Mzcoin) Type() string {
+	return Type
+}
