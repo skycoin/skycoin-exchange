37c5c960b360db71c15ecc1dc65159df273be0a0
iketheadore <heaven.mao@gmail.com>
2016-12-10 09:49:37 +0800

    Add mzcoin-node-addr flag

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -10,6 +10,7 @@ import (
 
 	logging "github.com/op/go-logging"
 	bitcoin "github.com/skycoin/skycoin-exchange/src/coin/bitcoin"
+	"github.com/skycoin/skycoin-exchange/src/coin/mzcoin"
 	skycoin "github.com/skycoin/skycoin-exchange/src/coin/skycoin"
 	"github.com/skycoin/skycoin-exchange/src/server"
 	"github.com/skycoin/skycoin/src/cipher"
@@ -38,12 +39,19 @@ func registerFlags(cfg *server.Config) {
 	flag.StringVar(&cfg.Seed, "seed", "", "wallet's seed")
 	flag.IntVar(&cfg.UtxoPoolSize, "poolsize", 1000, "utxo pool size")
 	flag.StringVar(&cfg.Admins, "admins", "", "admin pubkey list")
-	flag.StringVar(&cfg.SkycoinNodeAddr, "skycoin-node-addr", "127.0.0.1:6420", "skycoin node address")
+	var (
+		skyNodeAddr string
+		mzNodeAddr  string
+	)
+	flag.StringVar(&skyNodeAddr, "skycoin-node-addr", "127.0.0.1:6420", "skycoin node address")
+	flag.StringVar(&mzNodeAddr, "mzcoin-node-addr", "127.0.0.1:6430", "mzcoin node address")
 	flag.BoolVar(&cfg.HttpProf, "http-prof", false, "enable http profiling")
 	flag.StringVar(&cfg.Seckey, "seckey", "38d010a84c7b9374352468b41b076fa585d7dfac67ac34adabe2bbba4f4f6257", "private key used for encrypting and decryping messages")
 
 	flag.Set("logtostderr", "true")
 	flag.Parse()
+	cfg.NodeAddresses[skycoin.Type] = skyNodeAddr
+	cfg.NodeAddresses[mzcoin.Type] = mzNodeAddr
 }
 
 func main() {
@@ -56,13 +64,17 @@ func main() {
 	logger.Info("pubkey:%v", cipher.PubKeyFromSecKey(sk).Hex())
 
 	s := server.New(cfg)
-	s.BindCoins(&bitcoin.Bitcoin{}, &skycoin.Skycoin{NodeAddress: cfg.SkycoinNodeAddr})
+	// Bind supported coins
+	s.BindCoins(
+		&bitcoin.Bitcoin{},
+		&skycoin.Skycoin{NodeAddress: cfg.NodeAddresses[skycoin.Type]},
+		&mzcoin.Mzcoin{NodeAddress: cfg.NodeAddresses[mzcoin.Type]})
 	s.Run()
 }
 
-func initConfig() server.Config {
-	cfg := server.Config{}
-	registerFlags(&cfg)
+func initConfig() *server.Config {
+	cfg := server.NewConfig()
+	registerFlags(cfg)
 	if cfg.Seed == "" {
 		flag.Usage()
 		panic("seed must be set")
