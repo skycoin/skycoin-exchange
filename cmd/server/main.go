c330eaeb6e9ce49dc055831d706a3d7a92def62b
iketheadore <heaven.mao@gmail.com>
2017-05-23 23:34:43 +0800

    Update top file of server

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -9,9 +9,12 @@ import (
 	"net/http"
 
 	logging "github.com/op/go-logging"
+	"github.com/skycoin/skycoin-exchange/src/coin/aynrandcoin"
 	bitcoin "github.com/skycoin/skycoin-exchange/src/coin/bitcoin"
 	"github.com/skycoin/skycoin-exchange/src/coin/mzcoin"
+	"github.com/skycoin/skycoin-exchange/src/coin/shellcoin"
 	skycoin "github.com/skycoin/skycoin-exchange/src/coin/skycoin"
+	"github.com/skycoin/skycoin-exchange/src/coin/suncoin"
 	"github.com/skycoin/skycoin-exchange/src/server"
 	"github.com/skycoin/skycoin/src/cipher"
 )
@@ -40,11 +43,17 @@ func registerFlags(cfg *server.Config) {
 	flag.IntVar(&cfg.UtxoPoolSize, "poolsize", 1000, "utxo pool size")
 	flag.StringVar(&cfg.Admins, "admins", "", "admin pubkey list")
 	var (
-		skyNodeAddr string
-		mzNodeAddr  string
+		skyNodeAddr   string
+		mzNodeAddr    string
+		shellNodeAddr string
+		sunNodeAddr   string
+		aynNodeAddr   string
 	)
 	flag.StringVar(&skyNodeAddr, "skycoin-node-addr", "127.0.0.1:6420", "skycoin node address")
 	flag.StringVar(&mzNodeAddr, "mzcoin-node-addr", "127.0.0.1:7420", "mzcoin node address")
+	flag.StringVar(&shellNodeAddr, "shellcoin-node-addr", "127.0.0.1:7520", "suncoin node address")
+	flag.StringVar(&sunNodeAddr, "suncoin-node-addr", "127.0.0.1:7620", "suncoin node address")
+	flag.StringVar(&aynNodeAddr, "aynrandcoin-node-addr", "127.0.0.1:7720", "suncoin node address")
 	flag.BoolVar(&cfg.HTTPProf, "http-prof", false, "enable http profiling")
 	flag.StringVar(&cfg.Seckey, "seckey", "38d010a84c7b9374352468b41b076fa585d7dfac67ac34adabe2bbba4f4f6257", "private key used for encrypting and decryping messages")
 
@@ -52,6 +61,9 @@ func registerFlags(cfg *server.Config) {
 	flag.Parse()
 	cfg.NodeAddresses[skycoin.Type] = skyNodeAddr
 	cfg.NodeAddresses[mzcoin.Type] = mzNodeAddr
+	cfg.NodeAddresses[shellcoin.Type] = shellNodeAddr
+	cfg.NodeAddresses[suncoin.Type] = sunNodeAddr
+	cfg.NodeAddresses[aynrandcoin.Type] = aynNodeAddr
 }
 
 func main() {
@@ -68,7 +80,10 @@ func main() {
 	s.BindCoins(
 		&bitcoin.Bitcoin{},
 		skycoin.New(cfg.NodeAddresses[skycoin.Type]),
-		mzcoin.New(cfg.NodeAddresses[mzcoin.Type]))
+		mzcoin.New(cfg.NodeAddresses[mzcoin.Type]),
+		shellcoin.New(cfg.NodeAddresses[shellcoin.Type]),
+		aynrandcoin.New(cfg.NodeAddresses[aynrandcoin.Type]),
+		suncoin.New(cfg.NodeAddresses[suncoin.Type]))
 	s.Run()
 }
 
@@ -99,7 +114,7 @@ func initLogging(level logging.Level, color bool) {
 func initProfiling(httpProf bool) {
 	if httpProf {
 		go func() {
-			log.Println(http.ListenAndServe(":6060", nil))
+			log.Println(http.ListenAndServe(":6061", nil))
 		}()
 	}
 }
