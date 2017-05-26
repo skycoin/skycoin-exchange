8ed40925038d05202ae2de6630a54ba69aeaa08c
iketheadore <heaven.mao@gmail.com>
2017-05-26 12:00:52 +0800

    Add buid scripts

---
+++ build/build_android.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+gomobile bind -target=android github.com/skycoin/skycoin-exchange/src/api/mobile
+
---
+++ build/build_ios.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+gomobile bind -target=ios github.com/skycoin/skycoin-exchange/src/api/mobile
